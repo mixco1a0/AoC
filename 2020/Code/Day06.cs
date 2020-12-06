8a012064423c5b93dedb68a051aba7eea40587a6
mixco1a0 <mixco1a0@gmail.com>
2020-12-06 00:01:24 -0500

    starting day 06

---
+++ 2020/Code/Day06.cs
@@ -0,0 +1,22 @@
+
+using System.Collections.Generic;
+
+namespace _2020
+{
+    class Day06 : Day
+    {
+        public Day06() : base() {}
+        
+        protected override string GetDay() { return nameof(Day06).ToLower(); }
+
+        protected override void RunPart1Solution(List<string> inputs)
+        {
+            LogAnswer("NA");
+        }
+
+        protected override void RunPart2Solution(List<string>inputs)
+        {
+            LogAnswer("NA");
+        }
+    }
+}
