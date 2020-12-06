a2dcccdca410b54efdbcfa65abfeb4bb65ab6828
mixco1a0 <mixco1a0@gmail.com>
2020-12-05 23:20:56 -0500

    minor logging changes. adding exception handling.

--- 2020/Code/Day.cs
+++ 2020/Code/Day.cs
@@ -7,27 +7,43 @@ namespace _2020
 {
     abstract class Day
     {
-        private string m_curFile;
+        private string m_logID;
 
         protected abstract string GetDay();
         protected abstract void RunPart1Solution(List<string> inputs);
         protected abstract void RunPart2Solution(List<string> inputs);
 
+        private string DefaultLogID { get { return new string('.', 19); }}
+
         protected Day()
         {
-            m_curFile = "";
-            Run();
+            try
+            {
+                m_logID = DefaultLogID;
+                Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
 
         private void Run()
         {
-            SharedRun($"{GetDay()}_example.txt");
-            SharedRun($"{GetDay()}.txt");
+            SharedRun(false /* run example */);
+
+            // break up the logs a bit
+            LogFiller();
+            LogFiller();
+
+            SharedRun(true  /* run problem */);
         }
 
-        private void SharedRun(string fileName)
+        private void SharedRun(bool isProblem)
         {
-            m_curFile = fileName;
+            string fileName = string.Format("{0}{1}{2}", GetDay(), isProblem ? "" : "_example", ".txt");
+            m_logID = string.Format("{0}|{1}|partX", GetDay(), isProblem ? "problem" : "example");
 
             string inputFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", fileName);
             string[] rawFileRead = File.ReadAllText(inputFile).Split('\n').Select(str => str.Trim('\r')).ToArray();
@@ -38,46 +54,67 @@ namespace _2020
             RunPart1(inputs);
             timer.Stop();
             Log($"{timer.Print()}");
-            Log("");
+            LogFiller();
 
             inputs = rawFileRead.ToList();
             timer.Start();
             RunPart2(inputs);
             timer.Stop();
             Log($"{timer.Print()}");
-            Log("");
+            LogFiller();
 
-            m_curFile = "";
+            m_logID = DefaultLogID;
         }
         
         private void RunPart1(List<string> inputs)
         {
-            Log("Part 1");
-            RunPart1Solution(inputs);
+            m_logID = $"{m_logID[0..^1]}1";
+            try
+            {
+                RunPart1Solution(inputs);
+            }
+            catch (Exception e)
+            {
+                Log($"{e.Message}");
+                Log($"{e.StackTrace.Split('\r').FirstOrDefault()}");
+            }
         }
         
         private void RunPart2(List<string>inputs)
         {
-            Log("Part 2");
-            RunPart2Solution(inputs);
+            m_logID = $"{m_logID[0..^1]}2";
+            try
+            {
+                RunPart2Solution(inputs);
+            }
+            catch (Exception e)
+            {
+                Log($"{e.Message}");
+                Log($"{e.StackTrace.Split('\r').FirstOrDefault()}");
+            }
         }
 
         private void Log(string log)
         {
-            Console.WriteLine($"[{m_curFile}] {log}");
+            Console.WriteLine($"[{m_logID}] {log}");
+        }
+
+        private void LogFiller()
+        {
+            Console.WriteLine($"[{DefaultLogID}]");
         }
 
         protected void Debug(string log)
         {
-            Console.WriteLine($"[{m_curFile}] \t{log}");
+            Console.WriteLine($"[{m_logID}] \t{log}");
         }
 
         protected void LogAnswer(string answer)
         {
             string buffer = new string('*', answer.Length);
-            Console.WriteLine($"[{m_curFile}] *****{buffer}*****");
-            Console.WriteLine($"[{m_curFile}] ***  {answer}  ***");
-            Console.WriteLine($"[{m_curFile}] *****{buffer}*****");
+            Console.WriteLine($"[{m_logID}] *****{buffer}*****");
+            Console.WriteLine($"[{m_logID}] ***  {answer}  ***");
+            Console.WriteLine($"[{m_logID}] *****{buffer}*****");
         }
     }
 }
@@ -95,12 +132,12 @@ namespace _2020
 
         protected override void RunPart1Solution(List<string> inputs)
         {
-            LogAnswer("");
+            LogAnswer("NA");
         }
 
         protected override void RunPart2Solution(List<string>inputs)
         {
-            LogAnswer("");
+            LogAnswer("NA");
         }
     }
 }
