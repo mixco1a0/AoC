2dfef7e8bd554fb76e626b4c4b7ba74f7511b8cc
mixco1a0 <mixco1a0@gmail.com>
2020-12-07 22:18:32 -0500

    2015 day01 solution

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -5,16 +5,33 @@ namespace AoC
     class Program
     {
         static void Main(string[] args)
+        {            
+            Run2020();
+            Run2015();
+        }
+
+        static private void Run2020()
         {
             Console.WriteLine("");
-            
-            new _2020.Day01();
-            new _2020.Day02();
-            new _2020.Day03();
-            new _2020.Day04();
-            new _2020.Day05();
-            new _2020.Day06();
-            new _2020.Day07();
+            Console.WriteLine("Running 2020 Advent of Code");
+            Console.WriteLine("");
+
+            // new _2020.Day07();
+            // new _2020.Day06();
+            // new _2020.Day05();
+            // new _2020.Day04();
+            // new _2020.Day03();
+            // new _2020.Day02();
+            // new _2020.Day01();
+        }
+
+        static private void Run2015()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Running 2015 Advent of Code");
+            Console.WriteLine("");
+
+            new _2015.Day01();
         }
     }
 }
