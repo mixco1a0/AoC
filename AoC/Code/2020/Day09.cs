2c544b745a6f39e8e834d6172e16cd4fe64195cd
mixco1a0 <mixco1a0@gmail.com>
2020-12-09 00:28:20 -0500

    p2

--- AoC/Code/2020/Day09.cs
+++ AoC/Code/2020/Day09.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -47,23 +48,46 @@ namespace AoC._2020
             testData.Add(new TestDatum
             {
                 TestPart = TestPart.Two,
-                Output = "",
+                Output = "62",
                 RawInput =
-@""
+@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"
             });
             return testData;
         }
 
         protected override string RunPart1Solution(List<string> inputs)
         {
+            return GetWeakness(inputs, 25);
+        }
+
+        private string GetWeakness(List<string> inputs, int preamble)
+        {
             List<long> numbers = inputs.Select(num => long.Parse(num)).ToList();
-            const int preamble = 25;
             for (int i = preamble; i < numbers.Count(); ++i)
             {
                 if (!FindSum(numbers.Skip(i - preamble).Take(preamble).ToList(), numbers[i]))
                     return numbers[i].ToString();
             }
-            return "";
+            return "NaN";
         }
 
         private bool FindSum(List<long> list, long num)
@@ -82,7 +106,31 @@ namespace AoC._2020
 
         protected override string RunPart2Solution(List<string> inputs)
         {
-            return "";
+            try
+            {
+                long weakness = long.Parse(GetWeakness(inputs, 25));
+
+                List<long> numbers = inputs.Select(num => long.Parse(num)).ToList();
+                for (int i = 0; i < numbers.Count(); ++i)
+                {
+                    long runningTotal = numbers[i];
+                    for (int j = i + 1; j < numbers.Count(); ++j)
+                    {
+                        runningTotal += numbers[j];
+                        if (runningTotal == weakness)
+                        {
+                            List<long> subset = numbers.Skip(i).Take(j - i + 1).ToList();
+                            subset.Sort();
+                            return (subset.First() + subset.Last()).ToString();
+                        }
+                        else if (runningTotal > weakness)
+                            break;
+                    }
+                }
+            }
+            catch (Exception e) { }
+
+            return "NaN";
         }
-    }
+}
 }
