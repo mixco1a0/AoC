b12372bd1508975d3866037580f8929d39f09e2e
mixco1a0 <mixco1a0@gmail.com>
2020-12-09 00:14:54 -0500

    p1

--- AoC/Code/2020/Day09.cs
+++ AoC/Code/2020/Day09.cs
@@ -13,9 +13,36 @@ namespace AoC._2020
             testData.Add(new TestDatum
             {
                 TestPart = TestPart.One,
-                Output = "",
+                Output = "200",
                 RawInput =
-@""
+@"1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+49
+200"
             });
             testData.Add(new TestDatum
             {
@@ -29,7 +56,28 @@ namespace AoC._2020
 
         protected override string RunPart1Solution(List<string> inputs)
         {
+            List<long> numbers = inputs.Select(num => long.Parse(num)).ToList();
+            const int preamble = 25;
+            for (int i = preamble; i < numbers.Count(); ++i)
+            {
+                if (!FindSum(numbers.Skip(i - preamble).Take(preamble).ToList(), numbers[i]))
+                    return numbers[i].ToString();
+            }
             return "";
+        }
+
+        private bool FindSum(List<long> list, long num)
+        {
+            for (int i = 0; i < list.Count(); ++i)
+            {
+                for (int j = i + 1; j < list.Count(); ++j)
+                {
+                    if (list[i] + list[j] == num)
+                        return true;
+                }
+            }
+
+            return false;
         }
 
         protected override string RunPart2Solution(List<string> inputs)
