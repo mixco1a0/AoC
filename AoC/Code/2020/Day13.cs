1766c1ee0965c57322345ccf7812433cc93a2388
mixco1a0 <mixco1a0@gmail.com>
2020-12-13 00:16:43 -0500

    p1

--- AoC/Code/2020/Day13.cs
+++ AoC/Code/2020/Day13.cs
@@ -12,9 +12,10 @@ namespace AoC._2020
             testData.Add(new TestDatum
             {
                 TestPart = TestPart.One,
-                Output = "",
+                Output = "295",
                 RawInput =
-@""
+@"939
+7,13,x,x,59,x,31,19"
             });
             testData.Add(new TestDatum
             {
@@ -28,6 +29,22 @@ namespace AoC._2020
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
+            int time = int.Parse(inputs[0]);
+            List<int> buses = inputs[1].Split(new char[] { 'x', ',' }, System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> busWait = buses.Select(bus => time % bus).ToList();
+            int waitTime = 0;
+            while (true)
+            {
+                ++waitTime;
+                for (int i = 0; i < busWait.Count; ++i)
+                {
+                    busWait[i] = (busWait[i] + 1) % buses[i];
+                    if (busWait[i] == 0)
+                    {
+                        return (buses[i] * waitTime).ToString();
+                    }
+                }
+            }
             return "";
         }
 
