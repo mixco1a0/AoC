83bc26bd85dffed2dd4bafdcbbd950d92c5cffd4
mixco1a0 <mixco1a0@gmail.com>
2020-12-15 00:21:23 -0500

    p1

--- AoC/Code/2020/Day15.cs
+++ AoC/Code/2020/Day15.cs
@@ -12,9 +12,9 @@ namespace AoC._2020
             testData.Add(new TestDatum
             {
                 TestPart = TestPart.One,
-                Output = "",
+                Output = "436",
                 RawInput =
-@""
+@"0,3,6"
             });
             testData.Add(new TestDatum
             {
@@ -26,8 +26,49 @@ namespace AoC._2020
             return testData;
         }
 
+        class Turn
+        {
+            public int Index { get; set; }
+            public int Number { get; set; }
+            public override string ToString()
+            {
+                return $"{Number} @ {Index}";
+            }
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
+            List<int> numbers = inputs[0].Split(",").Select(int.Parse).ToList();
+            List<Turn> turns = new List<Turn>();
+            int index = 1;
+            foreach (int number in numbers)
+            {
+                turns.Add(new Turn { Index = index++, Number = number });
+            }
+            while (true)
+            {
+                Turn prevTurn = turns.Last();
+                int numCount = turns.Where(t => t.Number == prevTurn.Number).Count();
+                if (numCount == 1)
+                    turns.Add(new Turn { Index = index++, Number = 0 });
+                else
+                {
+                    List<Turn> temp = turns.Where(t => t.Number == prevTurn.Number).ToList();
+                    if (temp.Count == 1)
+                    {
+                        turns.Add(new Turn { Index = index++, Number = numCount });
+                    }
+                    else
+                    {
+                        Turn secLast = temp[temp.Count - 2];
+                        Turn last = temp[temp.Count - 1];
+                        turns.Add(new Turn { Index = index++, Number = last.Index - secLast.Index });
+                    }
+                }
+
+                if (turns.Last().Index == 2020)
+                    return turns.Last().Number.ToString();
+            }
             return "";
         }
 
