ef699f15bf5b1f188ea3fe7f8345975614c1ff3b
mixco1a0 <mixco1a0@gmail.com>
2020-12-15 00:54:59 -0500

    p1 was done so poorly, fixing that.

--- AoC/Code/2020/Day15.cs
+++ AoC/Code/2020/Day15.cs
@@ -26,55 +26,6 @@ namespace AoC._2020
             return testData;
         }
 
-        class Turn
-        {
-            public int Index { get; set; }
-            public int Number { get; set; }
-            public int LastIndex { get; set; }
-            public override string ToString()
-            {
-                return $"{Number} @ {Index}";
-            }
-        }
-
-        protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-        {
-            List<int> numbers = inputs[0].Split(",").Select(int.Parse).ToList();
-            List<Turn> turns = new List<Turn>();
-            int index = 1;
-            foreach (int number in numbers)
-            {
-                turns.Add(new Turn { Index = index++, Number = number });
-                // Debug($"Turn {turns.Last().Index} = {turns.Last().Number}");
-            }
-            while (true)
-            {
-                Turn prevTurn = turns.Last();
-                int numCount = turns.Where(t => t.Number == prevTurn.Number).Count();
-                if (numCount == 1)
-                    turns.Add(new Turn { Index = index++, Number = 0 });
-                else
-                {
-                    List<Turn> temp = turns.Where(t => t.Number == prevTurn.Number).ToList();
-                    if (temp.Count == 1)
-                    {
-                        turns.Add(new Turn { Index = index++, Number = numCount });
-                    }
-                    else
-                    {
-                        Turn secLast = temp[temp.Count - 2];
-                        Turn last = temp[temp.Count - 1];
-                        turns.Add(new Turn { Index = index++, Number = last.Index - secLast.Index });
-                    }
-                }
-
-                // Debug($"Turn {turns.Last().Index} = {turns.Last().Number}");
-
-                if (turns.Last().Index == 2020)
-                    return turns.Last().Number.ToString();
-            }
-        }
-
         class TurnInfo
         {
             public long Index { get; set; }
@@ -92,6 +43,34 @@ namespace AoC._2020
             }
         }
 
+        protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
+        {
+            List<int> numbers = inputs[0].Split(",").Select(int.Parse).ToList();
+            Dictionary<long, TurnInfo> turns = new Dictionary<long, TurnInfo>();
+            long index = 1;
+            long prevNumber = 0;
+            foreach (int number in numbers)
+            {
+                turns[number] = new TurnInfo { Index = index++, PrevIndex = -1 };
+                prevNumber = number;
+            }
+
+            while (true)
+            {
+                TurnInfo turnInfo = turns[prevNumber];
+                long curNumber = turnInfo.GetNext();
+                if (index == 2020)
+                    return curNumber.ToString();
+
+                if (!turns.ContainsKey(curNumber))
+                    turns[curNumber] = new TurnInfo { Index = index++, PrevIndex = -1 };
+                else
+                    turns[curNumber].Bump(index++);
+
+                prevNumber = curNumber;
+            }
+        }
+
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
         {
             List<int> numbers = inputs[0].Split(",").Select(int.Parse).ToList();
@@ -106,10 +85,8 @@ namespace AoC._2020
 
             while (true)
             {
-
                 TurnInfo turnInfo = turns[prevNumber];
                 long curNumber = turnInfo.GetNext();
-                // Debug($"Turn {index} = {curNumber}");
                 if (index == 30000000)
                     return curNumber.ToString();
 
@@ -119,8 +96,6 @@ namespace AoC._2020
                     turns[curNumber].Bump(index++);
 
                 prevNumber = curNumber;
-
-
             }
         }
     }
