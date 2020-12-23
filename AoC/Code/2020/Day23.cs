618cc1c5ffc7f52f66c65554e079eba19b2db3e8
mixco1a0 <mixco1a0@gmail.com>
2020-12-23 00:34:00 -0500

    p1

--- AoC/Code/2020/Day23.cs
+++ AoC/Code/2020/Day23.cs
@@ -25,9 +25,9 @@ namespace AoC._2020
             testData.Add(new TestDatum
             {
                 TestPart = TestPart.One,
-                Output = "",
+                Output = "67384529",
                 RawInput =
-@""
+@"389125467"
             });
             testData.Add(new TestDatum
             {
@@ -41,7 +41,47 @@ namespace AoC._2020
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            List<int> cups = inputs[0].ToCharArray().Select(c => int.Parse(c.ToString())).ToList();
+
+            int curCupIdx = 0;
+            int offset = 0;
+            for (int i = 0; i < 100; ++i)
+            {
+                DebugWriteLine($"-- move {i + 1} --");
+                DebugWriteLine($"cups: {string.Join(",", cups)}");
+                List<int> removedCups = new List<int>(cups.GetRange(curCupIdx + 1, 3));
+                DebugWriteLine($"cups: {string.Join(",", removedCups)}");
+                cups.RemoveRange(curCupIdx + 1, 3);
+
+                int destIdx = -1;
+                for (int c = 1; c < 10; ++c)
+                {
+                    int cupCount = cups.Where(cup => cup == cups[curCupIdx] - c).Count();
+                    if (cupCount == 1)
+                    {
+                        DebugWriteLine($"destination: {cups[curCupIdx] - c}");
+                        destIdx = cups.IndexOf(cups[curCupIdx] - c);
+                        break;
+                    }
+                }
+                if (destIdx < 0)
+                {
+                    DebugWriteLine($"destination: {cups.Max()}");
+                    destIdx = cups.IndexOf(cups.Max());
+                }
+                cups.InsertRange(destIdx + 1, removedCups);
+                cups.Add(cups.First());
+                cups.RemoveAt(curCupIdx);
+                ++offset;
+            }
+
+            while (cups[0] != 1)
+            {
+                cups.Add(cups.First());
+                cups.RemoveAt(0);
+            }
+
+            return string.Join("", cups);
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
