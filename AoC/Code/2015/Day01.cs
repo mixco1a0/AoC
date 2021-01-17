06b7e62044a5f42d3a2a8034d212d7c1ca35932e
mixco1a0 <mixco1a0@gmail.com>
2021-01-17 11:11:16 -0500

    adding v1 for first 9 days of 2015.

--- AoC/Code/2015/Day01.cs
+++ AoC/Code/2015/Day01.cs
@@ -6,7 +6,18 @@ namespace AoC._2015
     class Day01 : Day
     {
         public Day01() { }
-
+        public override string GetSolutionVersion(Part part)
+        {
+            switch (part)
+            {
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
+                default:
+                    return base.GetSolutionVersion(part);
+            }
+        }
         protected override List<TestDatum> GetTestData()
         {
             List<TestDatum> testData = new List<TestDatum>();
--- AoC/Code/2015/Day05.cs
+++ AoC/Code/2015/Day05.cs
@@ -6,7 +6,18 @@ namespace AoC._2015
     class Day05 : Day
     {
         public Day05() { }
-
+        public override string GetSolutionVersion(Part part)
+        {
+            switch (part)
+            {
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
+                default:
+                    return base.GetSolutionVersion(part);
+            }
+        }
         protected override List<TestDatum> GetTestData()
         {
             List<TestDatum> testData = new List<TestDatum>();
