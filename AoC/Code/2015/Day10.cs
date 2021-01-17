1949f5beac81d45224e203831d10497d7d9910de
mixco1a0 <mixco1a0@gmail.com>
2021-01-17 17:51:38 -0500

    p2

--- AoC/Code/2015/Day10.cs
+++ AoC/Code/2015/Day10.cs
@@ -6,6 +6,7 @@ namespace AoC._2015
 {
     class Day10 : Day
     {
+        private static int sTimes = 40;
         public Day10() { }
         public override string GetSolutionVersion(Part part)
         {
@@ -25,28 +26,67 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Variables = new Dictionary<string, string> { { nameof(sTimes), "5" } },
+                Output = "6",
                 RawInput =
-@""
-            });
-            testData.Add(new TestDatum
-            {
-                TestPart = Part.Two,
-                Output = "",
-                RawInput =
-@""
+@"1"
             });
             return testData;
         }
 
+        private string Process(string input)
+        {
+            string processed = "";
+            char cur = input.First();
+            int count = 1;
+            foreach (char c in input[1..])
+            {
+                if (c == cur)
+                {
+                    ++count;
+                }
+                else
+                {
+                    processed += $"{count}{cur}";
+                    cur = c;
+                    count = 1;
+                }
+            }
+            processed += $"{count}{cur}";
+            // DebugWriteLine($"Before:{input} - After:{processed}");
+            return processed;
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int times = 40;
+            if (variables != null && variables.ContainsKey(nameof(sTimes)))
+            {
+                times = int.Parse(variables[nameof(sTimes)]);
+            }
+            string input = inputs.First();
+            for (int i = 0; i < times; ++i)
+            {
+                input = Process(input);
+                DebugWriteLine($"{i} complete [{input.Length}]");
+            }
+            return input.Length.ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int times = 50;
+            if (variables != null && variables.ContainsKey(nameof(sTimes)))
+            {
+                times = int.Parse(variables[nameof(sTimes)]);
+            }
+            string input = inputs.First();
+            for (int i = 0; i < times; ++i)
+            {
+                input = Process(input);
+                DebugWriteLine($"{i} complete [{input.Length}]");
+            }
+            return input.Length.ToString();
         }
     }
 }
