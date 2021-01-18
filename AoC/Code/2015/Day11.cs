35e0348cc8617dda4db56c720808091ead99e7c6
mixco1a0 <mixco1a0@gmail.com>
2021-01-18 14:35:17 -0500

    p1

--- AoC/Code/2015/Day11.cs
+++ AoC/Code/2015/Day11.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,9 +26,16 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "abcdffaa",
                 RawInput =
-@""
+@"abcdefgh"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "ghjaabcc",
+                RawInput =
+@"ghijklmn"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +47,97 @@ namespace AoC._2015
             return testData;
         }
 
+        private string Increment(string password)
+        {
+            StringBuilder newPassword = new StringBuilder(password);
+            if (password.Last() == 'z')
+            {
+                bool carryOver = true;
+                for (int i = newPassword.Length - 1; i >= 0 && carryOver; --i)
+                {
+                    if (newPassword[i] == 'z')
+                    {
+                        newPassword[i] = 'a';
+                    }
+                    else
+                    {
+                        newPassword[i] = (char)(password[i] + 1);
+                        carryOver = false;
+                    }
+                }
+            }
+            else
+            {
+                newPassword[password.Length - 1] = (char)(password.Last() + 1);
+            }
+            return newPassword.ToString();
+        }
+
+        private bool HasInvalid(string password)
+        {
+            return password.Contains('i') || password.Contains('o') || password.Contains('l');
+        }
+
+        private bool HasStraight(string password)
+        {
+            const int Straight = 3;
+            char[] p = password.ToCharArray();
+            int curStraight = 1;
+            char curP = '\0';
+            for (int i = 0; i < p.Length; ++i)
+            {
+                if (p[i] - curP == 1)
+                {
+                    ++curStraight;
+                }
+                else
+                {
+                    curStraight = 1;
+                }
+                curP = p[i];
+
+                if (curStraight == Straight)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool HasDoubles(string password)
+        {
+            const int Doubles = 2;
+            char[] p = password.ToCharArray();
+            int doublesCount = 0;
+            char curP = '\0';
+            for (int i = 0; i < p.Length; ++i)
+            {
+                if (curP == p[i])
+                {
+                    ++doublesCount;
+                    if (doublesCount >= Doubles)
+                    {
+                        return true;
+                    }
+                    curP = '\0';
+                }
+                else
+                {
+                    curP = p[i];
+                }
+            }
+            return false;
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            string password = inputs.First();
+            do
+            {
+                password = Increment(password);
+                // DebugWriteLine($"Checking {password}");
+            } while (HasInvalid(password) || !HasStraight(password) || !HasDoubles(password));
+            return password;
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
