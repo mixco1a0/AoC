de4c335410e7e441035fb9004be6cb0b2e18d3e6
mixco1a0 <mixco1a0@gmail.com>
2021-01-24 13:00:56 -0500

    p1

--- AoC/Code/2015/Day17.cs
+++ AoC/Code/2015/Day17.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ namespace AoC._2015
     class Day17 : Day
     {
         public Day17() { }
+        static private int sLiters = 150;
         public override string GetSolutionVersion(Part part)
         {
             switch (part)
@@ -25,9 +27,14 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Variables = new Dictionary<string, string> { { nameof(sLiters), "25" } },
+                Output = "4",
                 RawInput =
-@""
+@"20
+15
+10
+5
+5"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +46,83 @@ namespace AoC._2015
             return testData;
         }
 
+        private int GetTotal(IEnumerable<int> inputs)
+        {
+            int total = 0;
+            foreach (int input in inputs)
+            {
+                total += input;
+            }
+            return total;
+        }
+
+        private int TryNext(int result, List<int> inputs)
+        {
+            int total = 0;
+            int count = 0;
+            List<bool> bools = inputs.Select(_ => false).ToList();
+            if (inputs.Count > 0)
+            {
+                for (int i = 0; i < inputs.Count; ++i)
+                {
+                    // impossible to reach the total using remaining numbers, skip ahead
+                    if (total + GetTotal(inputs.Skip(i)) < result)
+                    {
+                        i = inputs.Count - 1;
+                    }
+
+                    total += inputs[i];
+                    if (total > result)
+                    {
+                        total -= inputs[i];
+                    }
+                    else if (total == result)
+                    {
+                        bools[i] = true;
+                        DebugWriteLine($"VALID: {string.Join(',', bools.Select((b, i) => new { b = b, i = i }).Where(pair => pair.b).Select(pair => $"{inputs[pair.i]}[#{pair.i}]"))}");
+                        bools[i] = false;
+                        total -= inputs[i];
+                        ++count;
+                    }
+                    else
+                    {
+                        bools[i] = true;
+                    }
+
+                    if (i + 1 == inputs.Count)
+                    {
+                        bools[i] = false;
+                        while (i >= 0 && !bools[i])
+                        {
+                            --i;
+                        }
+                        if (i < 0)
+                        {
+                            return count;
+                        }
+                        bools[i] = false;
+
+                        IEnumerable<int> used = inputs.Select((num, idx) => (num, idx)).Where(pair => bools[pair.idx]).Select(pair => pair.num);
+                        total = 0;
+                        foreach (int num in used)
+                        {
+                            total += num;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int liters = sLiters;
+            if (variables != null && variables.ContainsKey(nameof(sLiters)))
+            {
+                liters = int.Parse(variables[nameof(sLiters)]);
+            }
+
+            return TryNext(liters, inputs.Select(int.Parse).OrderByDescending(_ => _).ToList()).ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
