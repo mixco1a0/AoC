6468a86bc8d664df94a9710833efd8024481222d
mixco1a0 <mixco1a0@gmail.com>
2021-01-30 23:24:31 -0500

    p1

--- AoC/Code/2015/Day18.cs
+++ AoC/Code/2015/Day18.cs
@@ -7,6 +7,7 @@ namespace AoC._2015
     class Day18 : Day
     {
         public Day18() { }
+        static private int sSteps = 100;
         public override string GetSolutionVersion(Part part)
         {
             switch (part)
@@ -25,9 +26,15 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Variables = new Dictionary<string, string> { { nameof(sSteps), "4" } },
+                Output = "4",
                 RawInput =
-@""
+@".#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.."
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +46,35 @@ namespace AoC._2015
             return testData;
         }
 
+        private char GetLocationState(int x, int y, List<List<char>> lights)
+        {
+            int onCount = Util.ProcessIndexBorder(x, y, lights, '#');
+
+            switch (lights[x][y])
+            {
+                case '.':
+                    return onCount == 3 ? '#' : '.';
+                case '#':
+                    return onCount == 2 || onCount == 3 ? '#' : '.';
+            }
+
+            return '!';
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int steps = sSteps;
+            if (variables != null && variables.ContainsKey(nameof(sSteps)))
+            {
+                steps = int.Parse(variables[nameof(sSteps)]);
+            }
+
+            List<List<char>> lights = inputs.Select(a => a.ToCharArray().ToList()).ToList();
+            for (int i = 0; i < steps; ++i)
+            {
+                Util.ProcessGrid(ref lights, GetLocationState);
+            }
+            return string.Join("", lights.Select(c => string.Join("", c))).Replace(".", "").Replace("L", "").Count().ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
