db128bf22b6889d2e674e395dbbbae2d38201504
mixco1a0 <mixco1a0@gmail.com>
2021-02-13 22:11:49 -0500

    p1

--- AoC/Code/2015/Day19.cs
+++ AoC/Code/2015/Day19.cs
@@ -25,9 +25,24 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "4",
                 RawInput =
-@""
+@"H => HO
+H => OH
+O => HH
+
+HOH"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "7",
+                RawInput =
+@"H => HO
+H => OH
+O => HH
+
+HOHOHO"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +54,38 @@ namespace AoC._2015
             return testData;
         }
 
+        public record Replacement (string pre, string post);
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            List<Replacement> replacements = new List<Replacement>();
+            string original = "";
+            foreach (string input in inputs)
+            {
+                if (input.Contains("=>"))
+                {
+                    string[] split = input.Split("=>", StringSplitOptions.RemoveEmptyEntries).Select(_ => _.Trim()).ToArray();
+                    replacements.Add(new Replacement(split[0], split[1]));
+                }
+                else if (input.Trim().Length > 0)
+                {
+                    original = input;
+                }
+            }
+
+            HashSet<string> unique = new HashSet<string>();
+            foreach (Replacement replacement in replacements)
+            {
+                for (int i = original.IndexOf(replacement.pre); i >= 0 && i < original.Length; i = original.IndexOf(replacement.pre, i + 1))
+                {
+                    string cur = original.Remove(i, replacement.pre.Length).Insert(i, replacement.post);
+                    if (!unique.Contains(cur))
+                    {
+                        unique.Add(cur);
+                    }
+                }
+            }
+            return unique.Count.ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
