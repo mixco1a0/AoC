69ca934d703bcbce9ec2be41402ca9ec7ad3221f
mixco1a0 <mixco1a0@gmail.com>
2021-02-16 22:56:53 -0500

    p1

--- AoC/Code/2015/Day20.cs
+++ AoC/Code/2015/Day20.cs
@@ -24,13 +24,6 @@ namespace AoC._2015
             List<TestDatum> testData = new List<TestDatum>();
             testData.Add(new TestDatum
             {
-                TestPart = Part.One,
-                Output = "",
-                RawInput =
-@""
-            });
-            testData.Add(new TestDatum
-            {
                 TestPart = Part.Two,
                 Output = "",
                 RawInput =
@@ -39,9 +32,57 @@ namespace AoC._2015
             return testData;
         }
 
+
+        // |10:29:18.636|  [2015|day20|problem|part1|v0] 	720720 -> 32497920
+        // |10:31:54.727|  [2015|day20|problem|part1|v0] 	776160 -> 33611760
+
+
+        private long GetStart(long lhn)
+        {
+            long temp = lhn;
+            for (long l = 1; ; ++l)
+            {
+                if (temp - l <= 0)
+                {
+                    return l;
+                }
+                temp -= l;
+            }
+        }
+
+        private long Sum(long end)
+        {
+            long sum = 0;
+            for (long i = 1; i <= end; ++i)
+            {
+                if (end % i == 0)
+                {
+                    sum += 10 * i;
+                }
+            }
+            return sum;
+        }
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            long lhn = inputs.Select(long.Parse).First();
+            long house = GetStart(lhn); house = 776160;
+            long max = 0, sum = 0;
+            while (true)
+            {
+                long now = Sum(house);
+                if (now > max)
+                {
+                    DebugWriteLine($"{house} -> {now}");
+                    max = now;
+                }
+                sum = now;
+                if (sum >= lhn)
+                {
+                    return house.ToString();
+                }
+                ++house;
+            }
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
