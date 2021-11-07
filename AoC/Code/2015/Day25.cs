b44f56379ee3ac970d9c76adc24cd5ca624687be
mixco1a0 <mixco1a0@gmail.com>
2021-11-07 01:14:46 -0500

    p1

--- AoC/Code/2015/Day25.cs
+++ AoC/Code/2015/Day25.cs
@@ -11,8 +11,8 @@ namespace AoC._2015
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
                 // case Part.Two:
                 //     return "v1";
                 default:
@@ -25,9 +25,9 @@ namespace AoC._2015
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "1601130",
                 RawInput =
-@""
+@"3 3"
             });
             testData.Add(new TestDatum
             {
@@ -41,7 +41,31 @@ namespace AoC._2015
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            List<int> coords = inputs.First().Split(" ,.".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Where(c => { int res; return int.TryParse(c, out res); }).Select(int.Parse).ToList();
+            long code = 20151125;
+            int targetR = coords[0];
+            int targetC = coords[1];
+            int c = 1, r = 1, maxR = 1;
+            while (true)
+            {
+                code = (code * 252533) % 33554393;
+                maxR = Math.Max(maxR, r);
+                if (--r < 1)
+                {
+                    r = maxR + 1;
+                    c = 1;
+                }
+                else
+                {
+                    c++;
+                }
+
+                if (targetR == r && targetC == c)
+                {
+                    break;
+                }
+            }
+            return code.ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
