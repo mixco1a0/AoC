e3ce5efe8efb33668c4f4a1a95e7f091e92d5124
mixco1a0 <mixco1a0@gmail.com>
2021-11-07 13:30:03 -0500

    p1

--- AoC/Code/2016/Day01.cs
+++ AoC/Code/2016/Day01.cs
@@ -25,9 +25,23 @@ namespace AoC._2016
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "5",
                 RawInput =
-@""
+@"R2, L3"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "2",
+                RawInput =
+@"R2, R2, R2"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "12",
+                RawInput =
+@"R5, L5, R5, R3"
             });
             testData.Add(new TestDatum
             {
@@ -41,7 +55,33 @@ namespace AoC._2016
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int coordX = 0, coordY = 0, curDirection = 0;
+            string[] input = inputs[0].Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            List<KeyValuePair<char, int>> instructions = new List<KeyValuePair<char, int>>();
+            foreach (string i in input)
+            {
+                curDirection += (i[0] == 'R' ? 1 : -1);
+                if (curDirection < 0)
+                {
+                    curDirection += 4;
+                }
+                switch (curDirection % 4)
+                {
+                    case 0:
+                        coordY += int.Parse(i[1..]);
+                        break;
+                    case 1:
+                        coordX += int.Parse(i[1..]);
+                        break;
+                    case 2:
+                        coordY -= int.Parse(i[1..]);
+                        break;
+                    case 3:
+                        coordX -= int.Parse(i[1..]);
+                        break;
+                }
+            }
+            return (Math.Abs(coordX) + Math.Abs(coordY)).ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
