f2c3562ad85baf6d4ac24286d8297b2d3bd83b43
mixco1a0 <mixco1a0@gmail.com>
2021-11-08 13:28:39 -0500

    p2

--- AoC/Code/2016/Day02.cs
+++ AoC/Code/2016/Day02.cs
@@ -12,10 +12,10 @@ namespace AoC._2016
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -36,17 +36,21 @@ UUUUD"
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "5DB3",
                 RawInput =
-@""
+@"ULL
+RRDDD
+LURDL
+UUUUD"
             });
             return testData;
         }
 
-        static string[] numberPad = {"123", "456", "789"};
+        static string[] numberPad1 = { "123", "456", "789" };
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
+            MinMax range = new MinMax(0, 2);
             StringBuilder code = new StringBuilder();
             int x = 1, y = 1;
             foreach (string input in inputs)
@@ -56,27 +60,61 @@ UUUUD"
                     switch (c)
                     {
                         case 'U':
-                            y = Math.Max(0, y - 1);
+                            y = Math.Max(range.Min, y - 1);
                             break;
                         case 'D':
-                            y = Math.Min(2, y + 1);
+                            y = Math.Min(range.Max, y + 1);
                             break;
                         case 'L':
-                            x = Math.Max(0, x - 1);
+                            x = Math.Max(range.Min, x - 1);
                             break;
                         case 'R':
-                            x = Math.Min(2, x + 1);
+                            x = Math.Min(range.Max, x + 1);
                             break;
                     }
                 }
-                code.Append(numberPad[y][x]);
+                code.Append(numberPad1[y][x]);
             }
             return code.ToString();
         }
 
+        static string[] numberPad2 = { "  1  ", " 234 ", "56789", " ABC ", "  D  " };
+
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            MinMax range = new MinMax(0, 4);
+            StringBuilder code = new StringBuilder();
+            int x = 0, y = 2;
+            foreach (string input in inputs)
+            {
+                foreach (char c in input)
+                {
+                    int oldX = x;
+                    int oldY = y;
+                    switch (c)
+                    {
+                        case 'U':
+                            y = Math.Max(range.Min, y - 1);
+                            break;
+                        case 'D':
+                            y = Math.Min(range.Max, y + 1);
+                            break;
+                        case 'L':
+                            x = Math.Max(range.Min, x - 1);
+                            break;
+                        case 'R':
+                            x = Math.Min(range.Max, x + 1);
+                            break;
+                    }
+                    if (numberPad2[y][x] == ' ')
+                    {
+                        x = oldX;
+                        y = oldY;
+                    }
+                }
+                code.Append(numberPad2[y][x]);
+            }
+            return code.ToString();
         }
     }
 }
