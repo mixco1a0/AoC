c37c07c36957e213cf93e02ba5dd977fb923294f
mixco1a0 <mixco1a0@gmail.com>
2021-11-07 21:19:25 -0500

    p2

--- AoC/Code/2016/Day01.cs
+++ AoC/Code/2016/Day01.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,10 @@ namespace AoC._2016
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -46,9 +47,9 @@ namespace AoC._2016
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "4",
                 RawInput =
-@""
+@"R8, R4, R4, R8"
             });
             return testData;
         }
@@ -57,7 +58,6 @@ namespace AoC._2016
         {
             int coordX = 0, coordY = 0, curDirection = 0;
             string[] input = inputs[0].Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            List<KeyValuePair<char, int>> instructions = new List<KeyValuePair<char, int>>();
             foreach (string i in input)
             {
                 curDirection += (i[0] == 'R' ? 1 : -1);
@@ -86,7 +86,54 @@ namespace AoC._2016
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int coordX = 0, coordY = 0, curDirection = 0;
+            string[] input = inputs[0].Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            List<Segment> visited = new List<Segment>();
+            Point prev = new Point(0, 0);
+            foreach (string i in input)
+            {
+                curDirection += (i[0] == 'R' ? 1 : -1);
+                if (curDirection < 0)
+                {
+                    curDirection += 4;
+                }
+                switch (curDirection % 4)
+                {
+                    case 0:
+                        coordY += int.Parse(i[1..]);
+                        break;
+                    case 1:
+                        coordX += int.Parse(i[1..]);
+                        break;
+                    case 2:
+                        coordY -= int.Parse(i[1..]);
+                        break;
+                    case 3:
+                        coordX -= int.Parse(i[1..]);
+                        break;
+                }
+                Segment cur = new Segment(prev, new Point(coordX, coordY));
+                //DebugWriteLine($"({cur.A.X,4},{cur.A.Y,4}) -> ({cur.B.X,4}, {cur.B.Y,4})");
+                Point intersection = null;
+                // check for intersection
+                foreach (Segment visit in visited.Take(visited.Count - 1))
+                {
+                    intersection = cur.GetIntersection(visit);
+                    if (intersection != null)
+                    {
+                        break;
+                    }
+                }
+                if (intersection != null)
+                {
+                    coordX = intersection.X;
+                    coordY = intersection.Y;
+                    break;
+                }
+                visited.Add(cur);
+                prev = cur.B;
+            }
+            return (Math.Abs(coordX) + Math.Abs(coordY)).ToString();
         }
     }
 }
