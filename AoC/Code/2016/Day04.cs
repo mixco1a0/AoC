73fcf7805e4882ae06c8ff82b02d879278684225
mixco1a0 <mixco1a0@gmail.com>
2021-11-09 09:48:37 -0500

    p1

--- AoC/Code/2016/Day04.cs
+++ AoC/Code/2016/Day04.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,9 +26,12 @@ namespace AoC._2016
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "1514",
                 RawInput =
-@""
+@"aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +43,55 @@ namespace AoC._2016
             return testData;
         }
 
+        private record Room(string Name, long SectorID, string Checksum)
+        {
+            static public Room Parse(string input)
+            {
+                string[] split = input.Split("-[],".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                StringBuilder sb = new StringBuilder();
+                split.SkipLast(2).ToList().ForEach(s => sb.Append(s));
+                return new Room(sb.ToString(), long.Parse(split.TakeLast(2).First()), split.Last());
+            }
+
+            private struct SortHelper
+            {
+                public char c;
+                public int count;
+            }
+
+            public bool IsValid()
+            {
+                SortHelper[] sorter = new SortHelper[26];
+                foreach(char c in Name)
+                {
+                    int index = c - 'a';
+                    sorter[c - 'a'].c = c;
+                    ++sorter[c - 'a'].count;
+                }
+                var postSort = sorter.OrderBy(s => s.c).OrderByDescending(s => s.count);
+                for (int i = 0; i < Checksum.Length; ++i)
+                {
+                    if (Checksum[i] != postSort.ElementAt(i).c)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        };
+
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            List<Room> allRooms = inputs.Select(Room.Parse).ToList();
+            long sectorIdSum = 0;
+            foreach(Room room in allRooms)
+            {
+                if (room.IsValid())
+                {
+                    sectorIdSum += room.SectorID;
+                }
+            }
+            return sectorIdSum.ToString();
         }
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
