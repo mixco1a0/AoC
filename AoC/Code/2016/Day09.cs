437ae50427ffc22e69cb78dcd3a324910c00004f
mixco1a0 <mixco1a0@gmail.com>
2021-11-12 23:30:23 -0500

    day09 p1

--- AoC/Code/2016/Day09.cs
+++ AoC/Code/2016/Day09.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,9 +26,44 @@ namespace AoC._2016
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "6",
                 RawInput =
-@""
+@"ADVENT"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "7",
+                RawInput =
+@"A(1x5)BC"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "9",
+                RawInput =
+@"(3x3)XYZ"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "11",
+                RawInput =
+@"A(2x2)BCD(2x2)EFG"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "6",
+                RawInput =
+@"(6x1)(1x3)A"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "18",
+                RawInput =
+@"X(8x2)(3x3)ABCY"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +75,44 @@ namespace AoC._2016
             return testData;
         }
 
+        public record SequenceCompression(int CharacterCount, int RepititionCount)
+        {
+            static public SequenceCompression Parse(string input)
+            {
+                string[] split = input.Split("(x)".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                return new SequenceCompression(int.Parse(split[0]), int.Parse(split[1]));
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            string compressedString = inputs.First();
+            StringBuilder sb = new StringBuilder();
+            for (int start = 0; start < compressedString.Length;)
+            {
+                int openP = compressedString.IndexOf('(', start);
+                if (openP == -1)
+                {
+                    sb.Append(compressedString.Substring(start));
+                    break;
+                }
+                else
+                {
+                    if (start < openP)
+                    {
+                        sb.Append(compressedString.Substring(start, openP - start));
+                    }
+                    int closeP = compressedString.IndexOf(')', openP) + 1;
+                    SequenceCompression sc = SequenceCompression.Parse(compressedString.Substring(openP, closeP - openP));
+                    string sequence = compressedString.Substring(closeP, sc.CharacterCount);
+                    for (int rep = 0; rep < sc.RepititionCount; ++rep)
+                    {
+                        sb.Append(sequence);
+                    }
+                    start = closeP + sc.CharacterCount;
+                }
+            }
+            return sb.ToString().Length.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
