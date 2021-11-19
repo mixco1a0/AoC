a2f6abd525c456acb2792c01b55e704e2a95b2b4
mixco1a0 <mixco1a0@gmail.com>
2021-11-19 11:36:42 -0500

    day15 prep

---
+++ AoC/Code/2016/Day15.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC._2016
+{
+    class Day15 : Day
+    {
+        public Day15() { }
+        public override string GetSolutionVersion(Part part)
+        {
+            switch (part)
+            {
+                // case Part.One:
+                //     return "v1";
+                // case Part.Two:
+                //     return "v1";
+                default:
+                    return base.GetSolutionVersion(part);
+            }
+        }
+        protected override List<TestDatum> GetTestData()
+        {
+            List<TestDatum> testData = new List<TestDatum>();
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Output = "",
+                RawInput =
+@""
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "",
+                RawInput =
+@""
+            });
+            return testData;
+        }
+
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        {
+            return "";
+        }
+
+        protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
+            => SharedSolution(inputs, variables);
+
+        protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
+            => SharedSolution(inputs, variables);
+    }
+}
