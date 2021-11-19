f38215a27fc73cc118760ade870cce6ebefa171a
mixco1a0 <mixco1a0@gmail.com>
2021-11-19 11:49:19 -0500

    day15 p1

--- AoC/Code/2016/Day15.cs
+++ AoC/Code/2016/Day15.cs
@@ -11,8 +11,8 @@ namespace AoC._2016
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
                 // case Part.Two:
                 //     return "v1";
                 default:
@@ -25,9 +25,10 @@ namespace AoC._2016
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "5",
                 RawInput =
-@""
+@"Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1."
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +40,38 @@ namespace AoC._2016
             return testData;
         }
 
+        private record Disk(int Id, int InitialPos, int MaxPositions)
+        {
+            static public Disk Parse(string input)
+            {
+                string[] split = input.Split(" #=,.".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                int[] values = split.Where(s => { int i; return int.TryParse(s, out i); }).Select(int.Parse).ToArray();
+                return new Disk(values[0], values[3], values[1]);
+            }
+            public int GetPosition(int time)
+            {
+                return (Id + InitialPos + time) % MaxPositions;
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            Disk[] disks = inputs.Select(Disk.Parse).ToArray();
+            int time = 0;
+            while (true)
+            {
+                bool fellThrough = true;
+                for (int d = 0; fellThrough && d < disks.Count(); ++d)
+                {
+                    fellThrough = (disks[d].GetPosition(time) == 0);
+                }
+                if (fellThrough)
+                {
+                    break;
+                }
+                ++time;
+            }
+            return time.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
