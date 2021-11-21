cc644961c9a9c871a945474be3ba87e018c17437
mixco1a0 <mixco1a0@gmail.com>
2021-11-20 22:50:15 -0500

    day18 p1

--- AoC/Code/2016/Day18.cs
+++ AoC/Code/2016/Day18.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,15 +20,25 @@ namespace AoC._2016
                     return base.GetSolutionVersion(part);
             }
         }
+
         protected override List<TestDatum> GetTestData()
         {
             List<TestDatum> testData = new List<TestDatum>();
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Variables = new Dictionary<string, string>() { { "rowCount", "3" } },
+                Output = "6",
                 RawInput =
-@""
+@"..^^."
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.One,
+                Variables = new Dictionary<string, string>() { { "rowCount", "10" } },
+                Output = "38",
+                RawInput =
+@".^^.^.^^^^"
             });
             testData.Add(new TestDatum
             {
@@ -39,9 +50,53 @@ namespace AoC._2016
             return testData;
         }
 
+        private char Safe { get { return '.'; } }
+        private char Trap { get { return '^'; } }
+
+        public char GetTile(string prevRow, int pos)
+        {
+            char l = Safe, c = Safe, r = Safe;
+
+            // left
+            if (pos != 0)
+            {
+                l = prevRow[pos - 1];
+            }
+
+            // center
+            c = prevRow[pos];
+
+            //right
+            if (pos < prevRow.Length - 1)
+            {
+                r = prevRow[pos + 1];
+            }
+
+            if (l != r)
+            {
+                return Trap;
+            }
+            return Safe;
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return "";
+            int rowCount;
+            Util.GetVariable(nameof(rowCount), 40, variables, out rowCount);
+            string prevRow = inputs.First();
+            StringBuilder allTiles = new StringBuilder();
+            for (int r = 0; r < rowCount; ++r)
+            {
+                DebugWriteLine($"Row {r,2} - {prevRow}");
+                allTiles.AppendLine(prevRow);
+                StringBuilder sb = new StringBuilder();
+                for (int c = 0; c < prevRow.Length; ++c)
+                {
+                    sb.Append(GetTile(prevRow, c));
+                }
+                prevRow = sb.ToString();
+            }
+            return allTiles.ToString().Where(c => c == Safe).Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
