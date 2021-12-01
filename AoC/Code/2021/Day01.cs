9a53077b3b315759e13d388462e96d65998f68ba
mixco1a0 <mixco1a0@gmail.com>
2021-12-01 00:14:44 -0500

    d01 p2

--- AoC/Code/2021/Day01.cs
+++ AoC/Code/2021/Day01.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,10 +11,10 @@ namespace AoC._2021
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -43,31 +42,43 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "5",
                 RawInput =
-@""
+@"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
             });
             return testData;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, int windowSize)
         {
             int[] depths = inputs.Select(int.Parse).ToArray();
             int increases = 0;
-            for (int i = 1; i < depths.Count(); ++i)
+            int prevSum = depths.Take(windowSize).Sum();
+            for (int i = 1; i <= depths.Count() - windowSize; ++i)
             {
-                if (depths[i] > depths[i-1])
+                int cur = depths.Skip(i).Take(windowSize).Sum();
+                if (cur > prevSum)
                 {
                     ++increases;
                 }
+                prevSum = cur;
             }
             return increases.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 1);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 3);
     }
 }
