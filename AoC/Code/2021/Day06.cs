e3ed3addb14d32151bd123da8d1e37bfb7896b94
mixco1a0 <mixco1a0@gmail.com>
2021-12-06 00:21:02 -0500

    day06 p2

--- AoC/Code/2021/Day06.cs
+++ AoC/Code/2021/Day06.cs
@@ -43,31 +43,68 @@ namespace AoC._2021
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, int days)
         {
-            List<int> fish = inputs.First().Split(',').Select(int.Parse).ToList();
-            for (int i = 0; i < days; ++i)
+            List<long> fish = inputs.First().Split(',').Select(long.Parse).ToList();
+            Dictionary<long, long> fishCounts = new Dictionary<long, long>();
+            foreach (long f in fish)
             {
-                int newFish = 0;
-                for (int f = 0; f < fish.Count; ++f)
+                if (!fishCounts.ContainsKey(f))
                 {
-                    --fish[f];
-                    if (fish[f] < 0)
+                    fishCounts[f] = 0;
+                }
+                ++fishCounts[f];
+            }
+            for (long i = 0; i < days; ++i)
+            {
+                Dictionary<long, long> nextState = new Dictionary<long, long>();
+                // int newFish = 0;
+                foreach (KeyValuePair<long, long> pair in fishCounts)
+                {
+                    long nextKey = pair.Key - 1;
+                    if (pair.Key - 1 < 0)
                     {
-                        fish[f] = 6;
-                        ++newFish;
+                        if (!nextState.ContainsKey(8))
+                        {
+                            nextState[8] = 0;
+                        }
+                        nextState[8] += pair.Value;
+
+                        if (!nextState.ContainsKey(6))
+                        {
+                            nextState[6] = 0;
+                        }
+                        nextState[6] += pair.Value;
+                    }
+                    else
+                    {
+                        if (!nextState.ContainsKey(nextKey))
+                        {
+                            nextState[nextKey] = 0;
+                        }
+                        nextState[nextKey] += pair.Value;
                     }
                 }
-                if (newFish > 0)
-                {
-                    fish.AddRange(Enumerable.Repeat(8, newFish));
-                }
+                fishCounts = nextState;
+                // for (int f = 0; f < fish.Count; ++f)
+                // {
+                //     --fish[f];
+                //     if (fish[f] < 0)
+                //     {
+                //         fish[f] = 6;
+                //         ++newFish;
+                //     }
+                // }
+                // if (newFish > 0)
+                // {
+                //     fish.AddRange(Enumerable.Repeat(8, newFish));
+                // }
             }
-            return fish.Count.ToString();
+            return fishCounts.Select(p => p.Value).Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
             => SharedSolution(inputs, variables, 80);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables, 0);
+            => SharedSolution(inputs, variables, 256);
     }
 }
