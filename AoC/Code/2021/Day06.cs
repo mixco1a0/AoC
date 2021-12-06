074b467d08eb2284d71899efae7c8baa24f1ede2
mixco1a0 <mixco1a0@gmail.com>
2021-12-06 00:42:00 -0500

    day06 cleanup

--- AoC/Code/2021/Day06.cs
+++ AoC/Code/2021/Day06.cs
@@ -12,10 +12,10 @@ namespace AoC._2021
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -34,18 +34,18 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "26984457539",
                 RawInput =
-@""
+@"3,4,3,1,2"
             });
             return testData;
         }
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, int days)
         {
-            List<long> fish = inputs.First().Split(',').Select(long.Parse).ToList();
+            List<long> fishUncrompressed = inputs.First().Split(',').Select(long.Parse).ToList();
             Dictionary<long, long> fishCounts = new Dictionary<long, long>();
-            foreach (long f in fish)
+            foreach (long f in fishUncrompressed)
             {
                 if (!fishCounts.ContainsKey(f))
                 {
@@ -53,50 +53,32 @@ namespace AoC._2021
                 }
                 ++fishCounts[f];
             }
+
+            Action<Dictionary<long, long>, long, long> UpdateCounts = (dictionary, key, value) =>
+            {
+                if (!dictionary.ContainsKey(key))
+                {
+                    dictionary[key] = 0;
+                }
+                dictionary[key] += value;
+            };
             for (long i = 0; i < days; ++i)
             {
                 Dictionary<long, long> nextState = new Dictionary<long, long>();
-                // int newFish = 0;
                 foreach (KeyValuePair<long, long> pair in fishCounts)
                 {
                     long nextKey = pair.Key - 1;
                     if (pair.Key - 1 < 0)
                     {
-                        if (!nextState.ContainsKey(8))
-                        {
-                            nextState[8] = 0;
-                        }
-                        nextState[8] += pair.Value;
-
-                        if (!nextState.ContainsKey(6))
-                        {
-                            nextState[6] = 0;
-                        }
-                        nextState[6] += pair.Value;
+                        UpdateCounts(nextState, 8, pair.Value);
+                        UpdateCounts(nextState, 6, pair.Value);
                     }
                     else
                     {
-                        if (!nextState.ContainsKey(nextKey))
-                        {
-                            nextState[nextKey] = 0;
-                        }
-                        nextState[nextKey] += pair.Value;
+                        UpdateCounts(nextState, nextKey, pair.Value);
                     }
                 }
                 fishCounts = nextState;
-                // for (int f = 0; f < fish.Count; ++f)
-                // {
-                //     --fish[f];
-                //     if (fish[f] < 0)
-                //     {
-                //         fish[f] = 6;
-                //         ++newFish;
-                //     }
-                // }
-                // if (newFish > 0)
-                // {
-                //     fish.AddRange(Enumerable.Repeat(8, newFish));
-                // }
             }
             return fishCounts.Select(p => p.Value).Sum().ToString();
         }
