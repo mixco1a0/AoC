193221a8f75fc71a6afb580f10a4fbe5858323c4
mixco1a0 <mixco1a0@gmail.com>
2021-12-08 02:06:14 -0500

    day08 p2

--- AoC/Code/2021/Day08.cs
+++ AoC/Code/2021/Day08.cs
@@ -43,25 +43,41 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "5353",
                 RawInput =
-@""
+@"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "61229",
+                RawInput =
+@"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
             });
             return testData;
         }
 
-        private Dictionary<int, int> DigitCount = new Dictionary<int, int>() 
+        private Dictionary<int, string> Digits = new Dictionary<int, string>()
         {
-            {0, 6},
-            {1, 2},
-            {2, 5},
-            {3, 5},
-            {4, 4},
-            {5, 5},
-            {6, 6},
-            {7, 3},
-            {8, 7},
-            {9, 6},
+            {0, "abcefg"},
+            {1, "cf"},
+            {2, "acdeg"},
+            {3, "acdfg"},
+            {4, "bcdf"},
+            {5, "abdfg"},
+            {6, "abdefg"},
+            {7, "acf"},
+            {8, "abcdefg"},
+            {9, "abcdfg"},
         };
 
         private class Signal
@@ -79,23 +95,112 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
             }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private int Solve(Signal signal, List<int> uniqueValues, List<int> uniqueDigits)
+        {
+            var test = Digits.Values.GroupBy(k => k.Length).Select(g => g).OrderBy(g => g.Count()).ToList();
+            const string all = "abcdefg";
+            Dictionary<char, string> translation = new Dictionary<char, string>();
+            for (char i = 'a'; i <= 'g'; ++i)
+            {
+                translation[i] = all;
+            }
+            //signal.Patterns.OrderBy(p => D)
+            foreach (string pattern in signal.Patterns.Where(p => uniqueValues.Contains(p.Length)))
+            {
+                IEnumerable<char> toRemove = all.ToCharArray().Where(c => !pattern.Contains(c));
+                string potentialDigits = Digits.Values.Where(v => v.Length == pattern.Length).First();
+                foreach (char d in potentialDigits)
+                {
+                    foreach (char tr in toRemove)
+                    {
+                        translation[d] = translation[d].Replace($"{tr}", string.Empty);
+                    }
+                }
+                toRemove = all.ToCharArray().Where(c => pattern.Contains(c));
+                foreach (char d in all.Where(a => !potentialDigits.Contains(a)))
+                {
+                    foreach (char tr in toRemove)
+                    {
+                        translation[d] = translation[d].Replace($"{tr}", string.Empty);
+                    }
+                }
+            }
+            Dictionary<char, string> backup = new Dictionary<char, string>(translation);
+            Dictionary<char, string> working = new Dictionary<char, string>(translation);
+            foreach (string pattern in signal.Patterns.Where(p => !uniqueValues.Contains(p.Length)))
+            {
+                IEnumerable<string> potentialDigits = Digits.Values.Where(v => v.Length == pattern.Length);
+                foreach (string potentialDigit in potentialDigits)
+                {
+                    IEnumerable<char> toRemove = all.ToCharArray().Where(c => !pattern.Contains(c));
+                    foreach (char d in potentialDigit)
+                    {
+                        foreach (char tr in toRemove)
+                        {
+                            working[d] = working[d].Replace($"{tr}", string.Empty);
+                        }
+                    }
+                    toRemove = all.ToCharArray().Where(c => pattern.Contains(c));
+                    foreach (char d in all.Where(a => !potentialDigit.Contains(a)))
+                    {
+                        foreach (char tr in toRemove)
+                        {
+                            working[d] = working[d].Replace($"{tr}", string.Empty);
+                        }
+                    }
+
+                    if (working.Values.Any(v => v.Length == 0))
+                    {
+                        working = new Dictionary<char, string>(backup);
+                    }
+                    else
+                    {
+                        backup = new Dictionary<char, string>(working);
+                        break;
+                    }
+                }
+            }
+
+            string finalCode = string.Empty;
+            Dictionary<char, char> finalTranslate = new Dictionary<char, char>();
+            foreach (var pair in working)
+            {
+                finalTranslate[pair.Value[0]] = pair.Key;
+            }
+            foreach (string digit in signal.Output.Select(o => string.Concat(o.OrderBy(c => c))))
+            {
+                string converted = string.Concat(string.Join(string.Empty, digit.Select(d => finalTranslate[d])).OrderBy(c => c));
+                int cur = Digits.Where(p => p.Value == converted).Select(p => p.Key).First();
+                finalCode += $"{cur}";
+            }
+
+            return int.Parse(finalCode);
+        }
+
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool decode)
         {
             List<Signal> signals = inputs.Select(Signal.Parse).ToList();
-            List<int> uniqueValues = DigitCount.Values.GroupBy(k => k).Where(g => g.Count() == 1).Select(g => g.Key).ToList(); //.Select((d, i) => new {d, i}).Where(p => p.i == 1).Select(p => p.d).ToList();
-            List<int> uniqueDigits = DigitCount.Where(p => uniqueValues.Contains(p.Value)).Select(p => p.Key).ToList();
+            List<int> uniqueValues = Digits.Values.GroupBy(k => k.Length).Where(g => g.Count() == 1).Select(g => g.Key).ToList();
+            List<int> uniqueDigits = Digits.Where(p => uniqueValues.Contains(p.Value.Length)).Select(p => p.Key).ToList();
             int sum = 0;
-            foreach(Signal signal in signals)
+            foreach (Signal signal in signals)
             {
-                sum += signal.Output.Where(p => uniqueValues.Contains(p.Length)).Count();
+                if (decode)
+                {
+                    sum += Solve(signal, uniqueValues, uniqueDigits);
+                }
+                else
+                {
+                    sum += signal.Output.Where(p => uniqueValues.Contains(p.Length)).Count();
+                }
             }
             return sum.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
     }
 }
