718b2ee090dd5e7bce8f707e9b56a0947b8943b0
mixco1a0 <mixco1a0@gmail.com>
2021-12-09 00:17:51 -0500

    day09 p1

--- AoC/Code/2021/Day09.cs
+++ AoC/Code/2021/Day09.cs
@@ -27,9 +27,13 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "15",
                 RawInput =
-@""
+@"2199943210
+3987894921
+9856789892
+8767896789
+9899965678"
             });
             testData.Add(new TestDatum
             {
@@ -41,9 +45,56 @@ namespace AoC._2021
             return testData;
         }
 
+        private List<Coords> Movements = new List<Coords>
+        {
+            new Coords(-1, 0),
+            new Coords(1, 0),
+            new Coords(0, 1),
+            new Coords(0, -1),
+        };
+
+        private bool IsLowPoint(int[,] grid, int maxX, int maxY, int x, int y)
+        {
+            Coords cur = new Coords(x, y);
+            foreach (Coords movement in Movements)
+            {
+                Coords neighbor = cur + movement;
+                if (neighbor.X < 0 || neighbor.X >= maxX || neighbor.Y < 0 || neighbor.Y >= maxY)
+                {
+                    continue;
+                }
+                if (grid[cur.X, cur.Y] >= grid[neighbor.X, neighbor.Y])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            int maxX = inputs.First().Count();
+            int maxY = inputs.Count();
+            int[,] grid = new int[maxX,maxY];
+            for (int y = 0; y < maxY; ++y)
+            {
+                for (int x = 0; x < maxX; ++x)
+                {
+                    grid[x,y] = inputs[y][x] - '0';
+                }
+            }
+            List<int> lowPoints = new List<int>();
+            for (int y = 0; y < maxY; ++y)
+            {
+                for (int x = 0; x < maxX; ++x)
+                {
+                    if (IsLowPoint(grid, maxX, maxY, x, y))
+                    {
+                        lowPoints.Add(grid[x,y] + 1);
+                    }
+                }
+            }
+            return lowPoints.Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
