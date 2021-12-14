8aa4cbee2d802b85075c78b2b2099e318985f08a
mixco1a0 <mixco1a0@gmail.com>
2021-12-14 00:11:31 -0500

    day14 p1

--- AoC/Code/2021/Day14.cs
+++ AoC/Code/2021/Day14.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,9 +28,27 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                //Variables = new Dictionary<string, string>() { { "steps", "5" } },
+                Output = "1588",
                 RawInput =
-@""
+@"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"
             });
             testData.Add(new TestDatum
             {
@@ -43,7 +62,31 @@ namespace AoC._2021
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            int steps;
+            Util.GetVariable(nameof(steps), 10, variables, out steps);
+
+            string polymer = inputs.First();
+            Dictionary<string, char> insertionRules = new Dictionary<string, char>();
+            foreach (string input in inputs.Skip(2))
+            {
+                string[] split = input.Split(" ->".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                insertionRules[split[0]] = split[1][0];
+            }
+
+            for (int i = 0; i < steps; ++i)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < polymer.Length - 1; ++j)
+                {
+                    sb.Append(polymer[j]);
+                    sb.Append(insertionRules[polymer.Substring(j, 2)]);
+                    //sb.Append(polymer[j+1]);
+                }
+                sb.Append(polymer.Last());
+                polymer = sb.ToString();
+            }
+            IEnumerable<int> counts = polymer.GroupBy(c => c).Select(g => g.Count());
+            return (counts.Max() - counts.Min()).ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
