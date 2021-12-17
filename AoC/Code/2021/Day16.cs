5849131c54c5c48696993b552793b1debc0fcc99
mixco1a0 <mixco1a0@gmail.com>
2021-12-16 23:13:05 -0500

    day16 p2

--- AoC/Code/2021/Day16.cs
+++ AoC/Code/2021/Day16.cs
@@ -1,7 +1,7 @@
-using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AoC._2021
 {
@@ -13,10 +13,10 @@ namespace AoC._2021
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -56,66 +56,160 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.Two,
-                Output = "",
+                Output = "3",
                 RawInput =
-@""
+@"C200B40A82"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "54",
+                RawInput =
+@"04005AC33890"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "7",
+                RawInput =
+@"880086C3E88112"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "9",
+                RawInput =
+@"CE00C43D881120"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "1",
+                RawInput =
+@"D8005AC2A8F0"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "0",
+                RawInput =
+@"F600BC2D8F"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "0",
+                RawInput =
+@"9C005AC2F8F0"
+            });
+            testData.Add(new TestDatum
+            {
+                TestPart = Part.Two,
+                Output = "1",
+                RawInput =
+@"9C0141080250320F1802104A08"
             });
             return testData;
         }
 
         public enum PacketType : int
         {
-            Operator3 = 3,
-            Literal = 4,
-            Operator2 = 6,
+            Sum = 0,
+            Mult = 1,
+            Min = 2,
+            Max = 3,
+            Raw = 4,
+            GreaterThan = 5,
+            LessThan = 6,
+            EqualTo = 7
         }
 
         public interface IPacket
         {
             public int Version { get; }
-            public IPacket Parent { get; }
+            public PacketOperator Parent { get; }
+            public PacketType Type { get; }
+
+            public long Evaluate();
 
             // debug
             public int Level { get; }
         }
 
-        private class PacketLiteral : IPacket
+        public class PacketLiteral : IPacket
         {
             public int Version { get; set; }
-            public IPacket Parent { get; set; }
+            public PacketOperator Parent { get; set; }
+            public PacketType Type { get => PacketType.Raw; }
             public long Value { get; set; }
-            public int Level { get; set; }
+            public int Level { get => Parent != null ? Parent.Level + 1 : 0; }
 
-            public PacketLiteral(int version, IPacket parent, long value)
+            public PacketLiteral(int version, PacketOperator parent, long value)
             {
                 Version = version;
                 Parent = parent;
                 Value = value;
-                Level = 0;
                 if (Parent != null)
                 {
-                    Level = parent.Level + 1;
+                    Parent.SubPackets.Add(this);
                 }
             }
+
+            public long Evaluate() => Value;
         }
 
-        private class PacketOperator : IPacket
+        public class PacketOperator : IPacket
         {
             public int Version { get; set; }
             public List<IPacket> SubPackets { get; set; }
-            public IPacket Parent { get; set; }
-            public int Level { get; set; }
+            public PacketOperator Parent { get; set; }
+            public PacketType Type { get; private set; }
+            public int Level { get => Parent != null ? Parent.Level + 1 : 0; }
 
-            public PacketOperator(int version, IPacket parent)
+            public PacketOperator(int version, PacketOperator parent, PacketType type)
             {
                 Version = version;
                 Parent = parent;
                 SubPackets = new List<IPacket>();
-                Level = 0;
+                Type = type;
                 if (Parent != null)
                 {
-                    Level = parent.Level + 1;
+                    Parent.SubPackets.Add(this);
                 }
+            }
+
+            public long Evaluate()
+            {
+                switch (Type)
+                {
+                    case PacketType.Sum:
+                        return SubPackets.Sum(sp => sp.Evaluate());
+                    case PacketType.Mult:
+                        return SubPackets.Aggregate(1L, (count, packet) => count * packet.Evaluate());
+                    case PacketType.Min:
+                        return SubPackets.Min(sp => sp.Evaluate());
+                    case PacketType.Max:
+                        return SubPackets.Max(sp => sp.Evaluate());
+                    case PacketType.GreaterThan:
+                        if (SubPackets[0].Evaluate() > SubPackets[1].Evaluate())
+                        {
+                            return 1L;
+                        }
+                        return 0L;
+                    case PacketType.LessThan:
+                        if (SubPackets[0].Evaluate() < SubPackets[1].Evaluate())
+                        {
+                            return 1L;
+                        }
+                        return 0L;
+                    case PacketType.EqualTo:
+                        if (SubPackets[0].Evaluate() == SubPackets[1].Evaluate())
+                        {
+                            return 1L;
+                        }
+                        return 0L;
+                }
+                return 0L;
             }
         }
 
@@ -135,10 +229,10 @@ namespace AoC._2021
             literal = Convert.ToInt64(sb.ToString(), 2);
         }
 
-        private int ParsePackets(string binary, PacketOperator parent, ref List<IPacket> packets, int maxPackages)
+        private int ParsePackets(string binary, PacketOperator parent, ref List<IPacket> packets, int maxPackets)
         {
             int curPos = 0;
-            int curPackages = 0;
+            int curPackets = 0;
             const int headerSize = 6;
             while (curPos + headerSize + 1 < binary.Length)
             {
@@ -157,11 +251,7 @@ namespace AoC._2021
                     curPos += bitsUsed;
                     PacketLiteral packet = new PacketLiteral(packetVersion, parent, literal);
                     packets.Add(packet);
-                    if (parent != null)
-                    {
-                        parent.SubPackets.Add(packets.Last());
-                    }
-                    DebugWriteLine($"{new string('*', packet.Level * 3)}[{curPackages}] Literal: [v{packetVersion}] {packet.Value}");
+                    // DebugWriteLine($"{new string('*', packet.Level * 3)}[#{curPackets,2}][v{packetVersion}][{(PacketType)packetTypeId}] {packet.Value}");
                 }
                 else
                 {
@@ -169,25 +259,21 @@ namespace AoC._2021
                     int bitLabelLength = is15BitRep ? 15 : 11;
                     int packetLabel = Convert.ToInt32(binary.Substring(curPos, bitLabelLength), 2);
                     curPos += bitLabelLength;
-                    PacketOperator packet = new PacketOperator(packetVersion, parent);
+                    PacketOperator packet = new PacketOperator(packetVersion, parent, (PacketType)packetTypeId);
                     packets.Add(packet);
                     if (is15BitRep)
                     {
-                        DebugWriteLine($"{new string('*', packet.Level * 3)}[{curPackages}] Size Op: [v{packetVersion}] Length:{packetLabel}");
+                        // DebugWriteLine($"{new string('*', packet.Level * 3)}[#{curPackets,2}][v{packetVersion}][{(PacketType)packetTypeId}] Len:{packetLabel}");
                         curPos += ParsePackets(binary.Substring(curPos, packetLabel), packet, ref packets, int.MaxValue);
-                        if (parent != null && maxPackages == int.MaxValue)
-                        {
-                            break;
-                        }
                     }
                     else
                     {
-                        DebugWriteLine($"{new string('*', packet.Level * 3)}[{curPackages}] Reps Op: [v{packetVersion}] Amount:{packetLabel}");
+                        // DebugWriteLine($"{new string('*', packet.Level * 3)}[#{curPackets,2}][v{packetVersion}][{(PacketType)packetTypeId}] Rep:{packetLabel}");
                         curPos += ParsePackets(binary.Substring(curPos), packet, ref packets, packetLabel);
                     }
                 }
 
-                if (++curPackages >= maxPackages)
+                if (++curPackets >= maxPackets)
                 {
                     break;
                 }
@@ -195,7 +281,7 @@ namespace AoC._2021
             return curPos;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool evaulate)
         {
             string fullHex = inputs.First();
             Func<char, string> ConvertToBinary = (char hex) =>
@@ -205,16 +291,20 @@ namespace AoC._2021
             };
             string binary = string.Join("", fullHex.Select(h => ConvertToBinary(h)));
             List<IPacket> packets = new List<IPacket>();
-            DebugWriteLine($"Converting {fullHex}");
+            // DebugWriteLine($"Converting {fullHex}");
             ParsePackets(binary, null, ref packets, int.MaxValue);
 
+            if (evaulate)
+            {
+                return packets.First().Evaluate().ToString();
+            }
             return packets.Aggregate(0, (count, packet) => count + packet.Version).ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
     }
 }
