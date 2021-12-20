8a4d19a630a40342fb17684ead0021f2d519c4d4
mixco1a0 <mixco1a0@gmail.com>
2021-12-20 09:25:30 -0500

    day18 perf

--- AoC/Code/2021/Day18.cs
+++ AoC/Code/2021/Day18.cs
@@ -12,10 +12,10 @@ namespace AoC._2021
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Part.One:
+                    return "v1";
+                case Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
