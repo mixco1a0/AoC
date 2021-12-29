ca7bc344bd2e6601f56c6ac81f979bcfa7231479
mixco1a0 <mixco1a0@gmail.com>
2021-12-29 14:55:34 -0500

    day20 p1

--- AoC/Code/2021/Day20.cs
+++ AoC/Code/2021/Day20.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +30,13 @@ namespace AoC._2021
                 TestPart = Part.One,
                 Output = "",
                 RawInput =
-@""
+@"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###"
             });
             testData.Add(new TestDatum
             {
@@ -41,15 +48,92 @@ namespace AoC._2021
             return testData;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        static readonly char LightPixel = '#';
+        static readonly char DarkPixel = '.';
+        static readonly Core.Point[] PixelCheck = new Core.Point[] { new Core.Point(-1, -1), new Core.Point(0, -1), new Core.Point(1, -1),
+                                                                     new Core.Point(-1, 0), new Core.Point(0, 0), new Core.Point(1, 0),
+                                                                     new Core.Point(-1, 1), new Core.Point(0, 1), new Core.Point(1, 1) };
+
+        private char EnhancePixel(List<string> pixels, string algorithm, int x, int y, char defaultPixel)
         {
-            return string.Empty;
+            StringBuilder sb = new StringBuilder();
+            foreach (Core.Point curPixel in PixelCheck.Select(p => new Core.Point(p.X + x, p.Y + y)))
+            {
+                if (curPixel.Y < 0 || curPixel.Y >= pixels.Count || curPixel.X < 0 || curPixel.X >= pixels[y].Length)
+                {
+                    sb.Append(defaultPixel);
+                }
+                else
+                {
+                    sb.Append(pixels[curPixel.Y][curPixel.X]);
+                }
+            }
+
+            string binary = sb.Replace(DarkPixel, '0').Replace(LightPixel, '1').ToString();
+            int key = Convert.ToInt32(binary, 2);
+            return algorithm[key];
+        }
+
+        private string EnhancePixels(List<string> pixels, string algorithm, int y, char defaultPixel)
+        {
+            StringBuilder oldPixels = new StringBuilder(pixels[y]);
+            StringBuilder newPixels = new StringBuilder(new string(DarkPixel, oldPixels.Length));
+            for (int x = 0; x < pixels.First().Length; ++x)
+            {
+                newPixels[x] = EnhancePixel(pixels, algorithm, x, y, defaultPixel);
+            }
+            return newPixels.ToString();
+        }
+
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, int enhancementCount)
+        {
+            string algorithm = inputs.First();
+
+            List<string> pixels = new List<string>();
+            pixels.AddRange(inputs.Skip(2));
+
+            char[] defaultPixels = new char[2] { DarkPixel, DarkPixel };
+            if (algorithm[0] == LightPixel)
+            {
+                defaultPixels[0] = DarkPixel;
+                defaultPixels[1] = LightPixel;
+            }
+
+            for (int i = 0; i < enhancementCount; ++i)
+            {
+                // expand
+                int oldXSize = pixels.First().Length;
+                int oldYSize = pixels.Count;
+                for (int y = 0; y < oldYSize; ++y)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(defaultPixels[i % 2]);
+                    sb.Append(pixels[y]);
+                    sb.Append(defaultPixels[i % 2]);
+                    pixels[y] = sb.ToString();
+                }
+                int newXSize = oldXSize + 2;
+                int newYSize = oldYSize + 2;
+                pixels.Insert(0, new string(defaultPixels[i % 2], newXSize));
+                pixels.Add(new string(defaultPixels[i % 2], newXSize));
+
+                // ehnance
+                List<string> newPixels = new List<string>();
+                for (int y = 0; y < newYSize; ++y)
+                {
+                    newPixels.Add(EnhancePixels(pixels, algorithm, y, defaultPixels[i % 2]));
+                }
+                pixels = newPixels;
+            }
+            return string.Join(string.Empty, pixels).Count(c => c == LightPixel).ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 2);
+        // 5402 => too high
+        // 5220 => too low
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 0);
     }
 }
