cc2be461adc8c590a9bb5164867c2ba2787661a0
mixco1a0 <mixco1a0@gmail.com>
2021-12-29 21:41:18 -0500

    day21 p1

--- AoC/Code/2021/Day21.cs
+++ AoC/Code/2021/Day21.cs
@@ -27,9 +27,10 @@ namespace AoC._2021
             testData.Add(new TestDatum
             {
                 TestPart = Part.One,
-                Output = "",
+                Output = "739785",
                 RawInput =
-@""
+@"Player 1 starting position: 4
+Player 2 starting position: 8"
             });
             testData.Add(new TestDatum
             {
@@ -43,7 +44,33 @@ namespace AoC._2021
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            // zero based
+            int p1 = int.Parse($"{inputs.First().Last()}") - 1;
+            int p2 = int.Parse($"{inputs.Last().Last()}") - 1;
+            int p1Score = 0;
+            int p2Score = 0;
+            int diceVal = 0;
+            bool p1Turn = true;
+            int turnCount = 0;
+            while (p1Score < 1000 && p2Score < 1000)
+            {
+                ++turnCount;
+                int p = p1Turn ? p1 : p2;
+                p += ++diceVal + ++diceVal + ++diceVal;
+                if (p1Turn)
+                {
+                    p1 = p % 10;
+                    p1Score += p1 + 1;
+                }
+                else
+                {
+                    p2 = p % 10;
+                    p2Score += p2 + 1;
+                }
+                p1Turn = !p1Turn;
+                diceVal %= 100;
+            }
+            return (turnCount * 3 * (p1Score > p2Score ? p2Score : p1Score)).ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
