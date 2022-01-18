d4e0f05cc556b2c13a10076eb364adb1a03139bb
mixco1a0 <mixco1a0@gmail.com>
2022-01-17 22:22:35 -0500

    y2015 d05 cleanup, perf, and out file

--- AoC/Code/2015/Day05.cs
+++ AoC/Code/2015/Day05.cs
@@ -2,33 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using AoC.Core;
-
 namespace AoC._2015
 {
-    class Day05 : Day
+    class Day05 : Core.Day
     {
         public Day05() { }
 
-        public override string GetSolutionVersion(Part part)
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                case Part.One:
+                case Core.Part.One:
                     return "v2";
-                case Part.Two:
+                case Core.Part.Two:
                     return "v2";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
 
-        protected override List<TestDatum> GetTestData()
+        public override bool SkipTestData => true;
+
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Output = "2",
                 RawInput =
 @"ugknbfddgicrmopn
@@ -37,9 +37,9 @@ jchzalrnumimnmhp
 haegwjzuvuyypxyu
 dvszwmarrgswjxmb"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Output = "2",
                 RawInput =
 @"qjhvhtzxzqqjkmpb
