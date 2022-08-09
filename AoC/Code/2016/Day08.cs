4d504e451ac8be522b3dcd45de9dbbaeb9fad7a2
mixco1a0 <mixco1a0@gmail.com>
2022-08-08 21:26:12 -0400

    y2016 d08 cleanup, perf, and out file

--- AoC/Code/2016/Day08.cs
+++ AoC/Code/2016/Day08.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using AoC.Core;
-
 namespace AoC._2016
 {
-    class Day08 : Day
+    class Day08 : Core.Day
     {
         public Day08() { }
-        public override string GetSolutionVersion(Part part)
+
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                case Part.One:
+                case Core.Part.One:
                     return "v1";
-                case Part.Two:
+                case Core.Part.Two:
                     return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
-        protected override List<TestDatum> GetTestData()
+
+        public override bool SkipTestData => true;
+
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Variables = new Dictionary<string, string> { { "gridW", "7" }, { "gridH", "3" } },
                 Output = "6",
                 RawInput =
@@ -36,9 +38,9 @@ rotate column x=1 by 1
 rotate row y=0 by 4
 rotate column x=1 by 1"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Output = "",
                 RawInput =
 @""
