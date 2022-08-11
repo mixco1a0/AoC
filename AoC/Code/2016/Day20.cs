e39204f3e4add3eaff485f4a3b04fbff7c4c8904
mixco1a0 <mixco1a0@gmail.com>
2022-08-10 21:50:18 -0400

    y2016 d20 cleanup, perf, and out file

--- AoC/Code/2016/Day20.cs
+++ AoC/Code/2016/Day20.cs
@@ -2,33 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using AoC.Core;
-
 namespace AoC._2016
 {
-    class Day20 : Day
+    class Day20 : Core.Day
     {
         public Day20() { }
 
-        public override string GetSolutionVersion(Part part)
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Core.Part.One:
+                    return "v1";
+                case Core.Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
 
-        protected override List<TestDatum> GetTestData()
+        public override bool SkipTestData => true;
+
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Output = "3",
                 RawInput =
 @"5-8
@@ -36,9 +36,9 @@ namespace AoC._2016
 4-7
 4-5"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Variables = new Dictionary<string, string>() { { "minValid", "0" }, { "maxValid", "9" } },
                 Output = "1",
                 RawInput =
