25210ce23c89199e79c7f723fc7c826edc08a8a2
mixco1a0 <mixco1a0@gmail.com>
2022-08-10 21:52:22 -0400

    y2016 d21 cleanup, perf, and out file

--- AoC/Code/2016/Day21.cs
+++ AoC/Code/2016/Day21.cs
@@ -3,33 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using AoC.Core;
-
 namespace AoC._2016
 {
-    class Day21 : Day
+    class Day21 : Core.Day
     {
         public Day21() { }
 
-        public override string GetSolutionVersion(Part part)
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                case Part.One:
+                case Core.Part.One:
                     return "v1";
-                case Part.Two:
+                case Core.Part.Two:
                     return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
 
-        protected override List<TestDatum> GetTestData()
+        public override bool SkipTestData => true;
+
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Variables = new Dictionary<string, string>() { { "unscrambledPassword", "abcde" } },
                 Output = "decab",
                 RawInput =
@@ -42,9 +42,9 @@ move position 3 to position 0
 rotate based on position of letter b
 rotate based on position of letter d"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Variables = new Dictionary<string, string>() { { "unscrambledPassword", "decab" } },
                 Output = "abcde",
                 RawInput =
