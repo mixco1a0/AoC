e84eba3b63435610c2fe311ec5c6209eb456e8c7
mixco1a0 <mixco1a0@gmail.com>
2022-11-12 22:09:57 -0500

    y2020 d02 cleanup, perf, and out file

--- AoC/Code/2020/Day02.cs
+++ AoC/Code/2020/Day02.cs
@@ -2,40 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using AoC.Core;
-
 namespace AoC._2020
 {
-    class Day02 : Day
+    class Day02 : Core.Day
     {
         public Day02() { }
-        public override string GetSolutionVersion(Part part)
+
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                case Part.One:
+                case Core.Part.One:
                     return "v2";
-                case Part.Two:
+                case Core.Part.Two:
                     return "v2";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
-        protected override List<TestDatum> GetTestData()
+
+        public override bool SkipTestData => true;
+
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Output = "2",
                 RawInput =
 @"1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Output = "1",
                 RawInput =
 @"1-3 a: abcde
