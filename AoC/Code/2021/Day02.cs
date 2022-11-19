40fc7965c1f898581b7a92b6f3b139cc5b12f4b2
mixco1a0 <mixco1a0@gmail.com>
2022-11-18 23:12:47 -0500

    y2021 d02 cleanup and perf

--- AoC/Code/2021/Day02.cs
+++ AoC/Code/2021/Day02.cs
@@ -1,33 +1,33 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using AoC.Core;
-
 namespace AoC._2021
 {
-    class Day02 : Day
+    class Day02 : Core.Day
     {
         public Day02() { }
 
-        public override string GetSolutionVersion(Part part)
+        public override string GetSolutionVersion(Core.Part part)
         {
             switch (part)
             {
-                // case Part.One:
-                //     return "v1";
-                // case Part.Two:
-                //     return "v1";
+                case Core.Part.One:
+                    return "v1";
+                case Core.Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
+        
+        public override bool SkipTestData => true;
 
-        protected override List<TestDatum> GetTestData()
+        protected override List<Core.TestDatum> GetTestData()
         {
-            List<TestDatum> testData = new List<TestDatum>();
-            testData.Add(new TestDatum
+            List<Core.TestDatum> testData = new List<Core.TestDatum>();
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.One,
+                TestPart = Core.Part.One,
                 Output = "150",
                 RawInput =
 @"forward 5
@@ -37,9 +37,9 @@ up 3
 down 8
 forward 2"
             });
-            testData.Add(new TestDatum
+            testData.Add(new Core.TestDatum
             {
-                TestPart = Part.Two,
+                TestPart = Core.Part.Two,
                 Output = "900",
                 RawInput =
 @"forward 5
