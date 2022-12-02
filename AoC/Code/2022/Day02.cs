470c77e2e8d630f810785499b5626ecbacbbc175
mixco1a0 <mixco1a0@gmail.com>
2022-12-02 00:13:09 -0500

    d02 p1

--- AoC/Code/2022/Day02.cs
+++ AoC/Code/2022/Day02.cs
@@ -29,9 +29,11 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "15",
                 RawInput =
-@""
+@"A Y
+B X
+C Z"
             });
             testData.Add(new Core.TestDatum
             {
@@ -45,7 +47,56 @@ namespace AoC._2022
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            int score = 0;
+
+            Func<char, int> RPS = (char input) =>
+            {
+                switch (input)
+                {
+                    case 'A':
+                    case 'X':
+                        return 1;
+                    case 'B':
+                    case 'Y':
+                        return 2;
+                    case 'C':
+                    case 'Z':
+                        return 3;
+                }
+                return 0;
+            };
+
+            foreach (string input in inputs)
+            {
+                string[] split = input.Split(' ');
+                if (RPS(split[0].First()) == RPS(split[1].First()))
+                {
+                    score += 3;
+                }
+                else if (split[0] == "A")
+                {
+                    if (split[1] == "Y")
+                    {
+                        score += 6;
+                    }
+                }
+                else if (split[0] == "B")
+                {
+                    if (split[1] == "Z")
+                    {
+                        score += 6;
+                    }
+                }
+                else if (split[0] == "C")
+                {
+                    if (split[1] == "X")
+                    {
+                        score += 6;
+                    }
+                }
+                score += RPS(split[1].First());
+            }
+            return score.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
