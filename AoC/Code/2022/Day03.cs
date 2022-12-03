a4b3859acc5c27d7fd101803fde4ecee57e9c477
mixco1a0 <mixco1a0@gmail.com>
2022-12-03 00:09:23 -0500

    d03 p1

--- AoC/Code/2022/Day03.cs
+++ AoC/Code/2022/Day03.cs
@@ -29,9 +29,14 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "157",
                 RawInput =
-@""
+@"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
             });
             testData.Add(new Core.TestDatum
             {
@@ -45,7 +50,26 @@ namespace AoC._2022
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            // a = 97 -> -96
+            // A = 65 -> -38
+            int priorities = 0;
+            foreach(string input in inputs)
+            {
+                int size = input.Length/2;
+                string l = input.Substring(0, size);
+                string r = input.Substring(size);
+                char[] allShared = l.ToCharArray().Intersect(r.ToCharArray()).ToArray();
+                char shared = allShared[0];
+                if (shared >= 'a' && shared <= 'z')
+                {
+                    priorities += (int)shared - 96;
+                }
+                else
+                {
+                    priorities += (int)shared - 38;
+                }
+            }
+            return priorities.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
