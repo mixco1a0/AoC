490f9d6e0f3614179abf194e5350bbdc6e2d8c3a
mixco1a0 <mixco1a0@gmail.com>
2022-12-04 00:08:35 -0500

    d04 p1

--- AoC/Code/2022/Day04.cs
+++ AoC/Code/2022/Day04.cs
@@ -29,9 +29,14 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "2",
                 RawInput =
-@""
+@"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +48,19 @@ namespace AoC._2022
             return testData;
         }
 
+        static Base.Range[] Parse(string input)
+        {
+            Base.Range[] ranges = new Base.Range[2];
+            string[] split = input.Split("-,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            ranges[0] = new Base.Range(int.Parse(split[0]), int.Parse(split[1]));
+            ranges[1] = new Base.Range(int.Parse(split[2]), int.Parse(split[3]));
+            return ranges;
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<Base.Range[]> ranges = inputs.Select(Parse).ToList();
+            return ranges.Where(r => (r[0].HasInc(r[1].Min) && r[0].HasInc(r[1].Max)) || (r[1].HasInc(r[0].Min) && r[1].HasInc(r[0].Max))).Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
