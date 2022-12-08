8469ea7641e68a2ab1579dee3fb5dbba23154251
mixco1a0 <mixco1a0@gmail.com>
2022-12-08 01:19:54 -0500

    d08 p1

--- AoC/Code/2022/Day08.cs
+++ AoC/Code/2022/Day08.cs
@@ -29,9 +29,13 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "21",
                 RawInput =
-@""
+@"30373
+25512
+65332
+33549
+35390"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +47,59 @@ namespace AoC._2022
             return testData;
         }
 
+        private void FindVisibleX(ref HashSet<Base.Point> visiblePoints, ref List<string> grid, int start, int end, Func<int, int> iter)
+        {
+            int max = Math.Max(start, end);
+            for (int y = 0; y < grid.Count; ++y)
+            {
+                int maxVisible = -1;
+                for (int x = start; 0 <= x && x <= max; x = iter(x))
+                {
+                    int value = grid[y][x] - '0';
+                    Base.Point cur = new Base.Point(x, y);
+                    if (!visiblePoints.Contains(cur))
+                    {
+                        if (value > maxVisible)
+                        {
+                            visiblePoints.Add(cur);
+                        }
+                    }
+                    maxVisible = Math.Max(maxVisible, value);
+                }
+            }
+        }
+
+        private void FindVisibleY(ref HashSet<Base.Point> visiblePoints, ref List<string> grid, int start, int end, Func<int, int> iter)
+        {
+            int max = Math.Max(start, end);
+            for (int x = 0; x < grid[0].Length; ++x)
+            {
+                int maxVisible = -1;
+                for (int y = start; 0 <= y && y <= max; y = iter(y))
+                {
+                    int value = grid[y][x] - '0';
+                    Base.Point cur = new Base.Point(x, y);
+                    if (!visiblePoints.Contains(cur))
+                    {
+                        if (value > maxVisible)
+                        {
+                            visiblePoints.Add(cur);
+                        }
+                    }
+                    maxVisible = Math.Max(maxVisible, value);
+                }
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            HashSet<Base.Point> visiblePoints = new HashSet<Base.Point>();
+            List<string> grid = new List<string>(inputs);
+            FindVisibleY(ref visiblePoints, ref grid, 0, grid.Count - 1, (int i) => { return i + 1; });
+            FindVisibleY(ref visiblePoints, ref grid, grid.Count - 1, 0, (int i) => { return i - 1; });
+            FindVisibleX(ref visiblePoints, ref grid, 0, grid[0].Length - 1, (int i) => { return i + 1; });
+            FindVisibleX(ref visiblePoints, ref grid, grid[0].Length - 1, 0, (int i) => { return i - 1; });
+            return visiblePoints.Count.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
