06a88eec000d4240e9b91d15f706e1c18eab7573
mixco1a0 <mixco1a0@gmail.com>
2022-12-09 01:21:07 -0500

    d09 p2

--- AoC/Code/2022/Day09.cs
+++ AoC/Code/2022/Day09.cs
@@ -43,9 +43,30 @@ R 2"
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.Two,
-                Output = "",
+                Output = "1",
                 RawInput =
-@""
+@"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
+            });
+            testData.Add(new Core.TestDatum
+            {
+                TestPart = Core.Part.Two,
+                Output = "36",
+                RawInput =
+@"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
             });
             return testData;
         }
@@ -59,7 +80,7 @@ R 2"
             }
         }
 
-        private void Move(char direction, ref Base.Point head, ref Base.Point tail, ref HashSet<Base.Point> visited)
+        private void Move(char direction, ref Base.Point head)
         {
             switch (direction)
             {
@@ -76,9 +97,17 @@ R 2"
                     head.Y -= 1;
                     break;
             }
+        }
 
-            // only adjust tail if needed
-            if (Math.Abs(head.X - tail.X) > 1)
+        private void Adjust(char direction, ref Base.Point head, ref Base.Point tail)
+        {
+            bool adjustX = Math.Abs(head.X - tail.X) > 1;
+            bool adjustY = Math.Abs(head.Y - tail.Y) > 1;
+            if (adjustX && adjustY)
+            {
+                tail = new Base.Point(head.X + (head.X > tail.X ? -1 : 1), head.Y + (head.Y > tail.Y ? -1 : 1));
+            }
+            else if (adjustX)
             {
                 if (head.Y == tail.Y)
                 {
@@ -89,7 +118,7 @@ R 2"
                     tail = new Base.Point(head.X + (head.X > tail.X ? -1 : 1), head.Y);
                 }
             }
-            else if (Math.Abs(head.Y - tail.Y) > 1)
+            else if (adjustY)
             {
                 if (head.X == tail.X)
                 {
@@ -100,33 +129,89 @@ R 2"
                     tail = new Base.Point(head.X, head.Y + (head.Y > tail.Y ? -1 : 1));
                 }
             }
-
-            if (!visited.Contains(tail))
-            {
-                visited.Add(new (tail));
-            }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private void PrintRope(Base.Point[] rope)
+        {
+            int minX = Math.Min(rope.Min(r => r.X) - 1, -1);
+            int maxX = Math.Max(rope.Max(r => r.X) + 2, 2);
+            int minY = Math.Min(rope.Min(r => r.Y) - 2, -2);
+            int maxY = Math.Max(rope.Max(r => r.Y) + 1, 1);
+
+            char[][] grid = new char[maxY - minY][];
+            for (int y = 0; y < grid.Length; ++y)
+            {
+                grid[y] = new char[maxX - minX];
+                for (int x = 0; x < grid[y].Length; ++x)
+                {
+                    grid[y][x] = '.';
+                }
+            }
+
+            for (int r = 0; r < rope.Length; ++r)
+            {
+                int x = rope[r].X - minX;
+                int y = maxY - rope[r].Y;
+
+                if (grid[y][x] == '.')
+                {
+                    grid[y][x] = r.ToString()[0];
+                }
+            }
+
+            if (grid[maxY][0 - minX] == '.')
+            {
+                grid[maxY][0 - minX] = 's';
+            }
+
+            Util.Grid.PrintGrid(Core.Log.ELevel.Debug, grid);
+        }
+
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, int nodeCount)
         {
             Instruction[] instructions = inputs.Select(Instruction.Parse).ToArray();
-            Base.Point head = new Base.Point(0, 0);
-            Base.Point tail = new Base.Point(0, 0);
+            Base.Point[] rope = new Base.Point[nodeCount];
+            for (int r = 0; r < rope.Length; ++r)
+            {
+                rope[r] = new Base.Point(0, 0);
+            }
+            bool massPrint = false;
+
             HashSet<Base.Point> visited = new HashSet<Base.Point>();
             foreach (Instruction i in instructions)
             {
                 for (int s = 0; s < i.Steps; ++s)
                 {
-                    Move(i.Direction, ref head, ref tail, ref visited);
+                    Move(i.Direction, ref rope[0]);
+                    for (int r = 1; r < rope.Length; ++r)
+                    {
+                        Adjust(i.Direction, ref rope[r - 1], ref rope[r]);
+
+                        if (massPrint)
+                        {
+                            PrintRope(rope);
+                        }
+                    }
+
+                    if (!visited.Contains(rope.Last()))
+                    {
+                        visited.Add(new(rope.Last()));
+                    }
+
+                    if (nodeCount > 2)
+                    {
+                        // DebugWriteLine($"Moving: {i.Direction}");
+                        // PrintRope(rope);
+                    }
                 }
             }
             return visited.Count.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 2);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, 10);
     }
 }
