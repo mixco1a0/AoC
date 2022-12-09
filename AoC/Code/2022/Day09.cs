ce4e6c3f9b581871a6dc661af94cc5502fd94cfc
mixco1a0 <mixco1a0@gmail.com>
2022-12-09 00:25:15 -0500

    d09 p1

--- AoC/Code/2022/Day09.cs
+++ AoC/Code/2022/Day09.cs
@@ -29,9 +29,16 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "13",
                 RawInput =
-@""
+@"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +50,77 @@ namespace AoC._2022
             return testData;
         }
 
+        private record Instruction(char Direction, int Steps)
+        {
+            public static Instruction Parse(string input)
+            {
+                string[] split = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                return new Instruction(split[0][0], int.Parse(split[1]));
+            }
+        }
+
+        private void Move(char direction, ref Base.Point head, ref Base.Point tail, ref HashSet<Base.Point> visited)
+        {
+            switch (direction)
+            {
+                case 'R':
+                    head.X += 1;
+                    break;
+                case 'L':
+                    head.X -= 1;
+                    break;
+                case 'U':
+                    head.Y += 1;
+                    break;
+                case 'D':
+                    head.Y -= 1;
+                    break;
+            }
+
+            // only adjust tail if needed
+            if (Math.Abs(head.X - tail.X) > 1)
+            {
+                if (head.Y == tail.Y)
+                {
+                    tail.X += head.X > tail.X ? 1 : -1;
+                }
+                else
+                {
+                    tail = new Base.Point(head.X + (head.X > tail.X ? -1 : 1), head.Y);
+                }
+            }
+            else if (Math.Abs(head.Y - tail.Y) > 1)
+            {
+                if (head.X == tail.X)
+                {
+                    tail.Y += head.Y > tail.Y ? 1 : -1;
+                }
+                else
+                {
+                    tail = new Base.Point(head.X, head.Y + (head.Y > tail.Y ? -1 : 1));
+                }
+            }
+
+            if (!visited.Contains(tail))
+            {
+                visited.Add(new (tail));
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            Instruction[] instructions = inputs.Select(Instruction.Parse).ToArray();
+            Base.Point head = new Base.Point(0, 0);
+            Base.Point tail = new Base.Point(0, 0);
+            HashSet<Base.Point> visited = new HashSet<Base.Point>();
+            foreach (Instruction i in instructions)
+            {
+                for (int s = 0; s < i.Steps; ++s)
+                {
+                    Move(i.Direction, ref head, ref tail, ref visited);
+                }
+            }
+            return visited.Count.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
