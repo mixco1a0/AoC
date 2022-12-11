42913397d1991c613aadbedb6c939b5cab86a41c
mixco1a0 <mixco1a0@gmail.com>
2022-12-11 10:06:46 -0500

    d11 p2

--- AoC/Code/2022/Day11.cs
+++ AoC/Code/2022/Day11.cs
@@ -62,9 +62,35 @@ Monkey 3:
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.Two,
-                Output = "",
+                Output = "2713310158",
                 RawInput =
-@""
+@"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1"
             });
             return testData;
         }
@@ -77,45 +103,47 @@ Monkey 3:
 
         public class Monkey
         {
+            public static long LCDiv { get; set; }
             public int Id { get; set; }
-            public List<int> Items { get; set; }
+            public List<long> Items { get; set; }
             public EOp Op { get; set; }
             public bool UseOld { get; set; }
-            public int Value { get; set; }
-            public int Div { get; set; }
+            public long Value { get; set; }
+            public long Div { get; set; }
             public int True { get; set; }
             public int False { get; set; }
-            public int InspectionCount { get; set; }
+            public long InspectionCount { get; set; }
 
             public static Monkey Parse(List<string> input)
             {
                 Monkey monkey = new Monkey();
                 monkey.Id = int.Parse(input[0].Split(" :".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
-                monkey.Items = input[1].Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Where(s => int.TryParse(s, out int i)).Select(int.Parse).ToList();
+                monkey.Items = input[1].Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Where(s => long.TryParse(s, out long l)).Select(long.Parse).ToList();
                 monkey.Op = input[2].Contains('*') ? EOp.Mult : EOp.Add;
                 monkey.UseOld = input[2].Split(" +*".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last() == "old";
                 if (!monkey.UseOld)
                 {
-                    monkey.Value = int.Parse(input[2].Split(" +*".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
+                    monkey.Value = long.Parse(input[2].Split(" +*".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
                 }
-                monkey.Div = int.Parse(input[3].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
+                monkey.Div = long.Parse(input[3].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
                 monkey.True = int.Parse(input[4].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
                 monkey.False = int.Parse(input[5].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
                 monkey.InspectionCount = 0;
+                LCDiv *= monkey.Div;
                 return monkey;
             }
 
-            public int Worry()
+            public long Worry(bool relief)
             {
                 ++InspectionCount;
-                int oldV = Items.First();
+                long oldV = Items.First();
                 Items.RemoveAt(0);
-                int otherV = Value;
+                long otherV = Value;
                 if (UseOld)
                 {
                     otherV = oldV;
                 }
-                int newV = oldV;
+                long newV = oldV;
                 if (Op == EOp.Add)
                 {
                     newV += otherV;
@@ -124,13 +152,20 @@ Monkey 3:
                 {
                     newV *= otherV;
                 }
-                newV /= 3;
+                if (relief)
+                {
+                    newV /= 3;
+                }
+                else
+                {
+                    newV %= LCDiv;
+                }
                 return newV;
             }
 
             public override string ToString()
             {
-                return $"[{Id}] has {string.Join(',', Items)}. new = old {(char)Op} {string.Format("{0}", UseOld ? "old" : Value)} | %{Div} => T=>{True} F=>{False}";
+                return $"[{Id}@{InspectionCount}] has {string.Join(',', Items)}. new = old {(char)Op} {string.Format("{0}", UseOld ? "old" : Value)} | %{Div} => T=>{True} F=>{False}";
             }
         }
 
@@ -154,13 +189,13 @@ Monkey 3:
             return monkeys.ToArray();
         }
 
-        public void DoRound(ref Monkey[] monkeys)
+        public void DoRound(ref Monkey[] monkeys, bool relief)
         {
             foreach (Monkey monkey in monkeys)
             {
                 while (monkey.Items.Count != 0)
                 {
-                    int worry = monkey.Worry();
+                    long worry = monkey.Worry(relief);
                     if (worry % monkey.Div == 0)
                     {
                         monkeys[monkey.True].Items.Add(worry);
@@ -173,21 +208,22 @@ Monkey 3:
             }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool relief, int roundCount)
         {
+            Monkey.LCDiv = 1;
             Monkey[] monkeys = GetMonkeys(inputs);
-            for (int i = 0; i < 20; ++i)
+            for (int i = 0; i < roundCount; ++i)
             {
-                DoRound(ref monkeys);
+                DoRound(ref monkeys, relief);
             }
-            List<int> ic = monkeys.Select(m => m.InspectionCount).OrderByDescending(mic => mic).Take(2).ToList();
+            List<long> ic = monkeys.Select(m => m.InspectionCount).OrderByDescending(mic => mic).Take(2).ToList();
             return (ic[0] * ic[1]).ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true, 20);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false, 10000);
     }
 }
