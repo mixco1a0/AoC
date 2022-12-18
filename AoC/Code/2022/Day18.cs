0eebd99946a19b803cf5d5e94b03c5130000ce0a
mixco1a0 <mixco1a0@gmail.com>
2022-12-18 00:22:34 -0500

    day18 p1

--- AoC/Code/2022/Day18.cs
+++ AoC/Code/2022/Day18.cs
@@ -29,9 +29,29 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "10",
                 RawInput =
-@""
+@"1,1,1
+2,1,1"
+            });
+            testData.Add(new Core.TestDatum
+            {
+                TestPart = Core.Part.One,
+                Output = "64",
+                RawInput =
+@"2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +63,43 @@ namespace AoC._2022
             return testData;
         }
 
+        private record Position3(int X, int Y, int Z)
+        {
+            public static Position3 Parse(string input)
+            {
+                int[] split = input.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                return new Position3(split[0], split[1], split[2]);
+            }
+
+            public static Position3 operator +(Position3 a, Position3 b)
+            {
+                return new Position3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+            }
+        }
+
+        private readonly List<Position3> Movement = new List<Position3>()
+        {
+            new Position3(1, 0, 0),
+            new Position3(0, 1, 0),
+            new Position3(0, 0, 1),
+            new Position3(-1, 0, 0),
+            new Position3(0, -1, 0),
+            new Position3(0, 0, -1),
+        };
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<Position3> pos = inputs.Select(Position3.Parse).ToList();
+
+            List<Position3> unique = new List<Position3>();
+            foreach (Position3 p in pos)
+            {
+                foreach (Position3 m in Movement)
+                {
+                    unique.Add(p + m);
+                }
+            }
+            return unique.Where(u => !pos.Contains(u)).Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
