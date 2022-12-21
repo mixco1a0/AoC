73313a49344fb46959fd0b306f626e3e2b915031
mixco1a0 <mixco1a0@gmail.com>
2022-12-21 00:23:20 -0500

    d21 p1

--- AoC/Code/2022/Day21.cs
+++ AoC/Code/2022/Day21.cs
@@ -29,9 +29,23 @@ namespace AoC._2022
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "152",
                 RawInput =
-@""
+@"root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +57,92 @@ namespace AoC._2022
             return testData;
         }
 
+        internal enum EOp
+        {
+            Raw = ' ',
+            Add = '+',
+            Sub = '-',
+            Mult = '*',
+            Div = '/'
+        }
+
+        private class Monkey
+        {
+            public string Id { get; set; }
+            public string[] Others { get; set; }
+            public long Value { get; set; }
+            public EOp Op { get; set; }
+
+            public Monkey()
+            {
+                Id = string.Empty;
+                Others = new string[2];
+                Value = 0;
+                Op = EOp.Raw;
+            }
+
+            public static Monkey Parse(string input)
+            {
+                string[] split = input.Split(" :".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                Monkey m = new Monkey();
+                m.Id = split[0];
+                if (long.TryParse(split[1], out long val))
+                {
+                    m.Op = EOp.Raw;
+                    m.Value = val;
+                }
+                else
+                {
+                    m.Others[0] = split[1];
+                    m.Op = (EOp)split[2][0];
+                    m.Others[1] = split[3];
+                }
+                return m;
+            }
+
+            public long Perform(Dictionary<string, long> values)
+            {
+                long a = values[Others[0]];
+                long b = values[Others[1]];
+                switch (Op)
+                {
+                    case EOp.Add:
+                        return a + b;
+                    case EOp.Sub:
+                        return a - b;
+                    case EOp.Mult:
+                        return a * b;
+                    case EOp.Div:
+                        return a / b;
+                }
+                return 0;
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            Queue<Monkey> monkeys = new Queue<Monkey>(inputs.Select(Monkey.Parse));
+            Dictionary<string, long> values = new Dictionary<string, long>();
+            while (monkeys.Count > 0)
+            {
+                Monkey monkey = monkeys.Dequeue();
+                if (monkey.Op == EOp.Raw)
+                {
+                    DebugWriteLine($"{monkey.Id} => {monkey.Value}");
+                    values[monkey.Id] = monkey.Value;
+                    continue;
+                }
+
+                if (values.ContainsKey(monkey.Others[0]) && values.ContainsKey(monkey.Others[1]))
+                {
+                    values[monkey.Id] = monkey.Perform(values);
+                    DebugWriteLine($"{monkey.Id} => {monkey.Others[0]} [{values[monkey.Others[0]]}] {(char)monkey.Op} {monkey.Others[1]} [{values[monkey.Others[1]]}] = {values[monkey.Id]}");
+                    continue;
+                }
+
+                monkeys.Enqueue(monkey);
+            }
+            return values["root"].ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
