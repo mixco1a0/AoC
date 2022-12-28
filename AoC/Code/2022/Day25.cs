14fcd741babd38b3384edd19519c03c70a1207ec
mixco1a0 <mixco1a0@gmail.com>
2022-12-28 14:56:12 -0500

    d25 perf

--- AoC/Code/2022/Day25.cs
+++ AoC/Code/2022/Day25.cs
@@ -13,39 +13,20 @@ namespace AoC._2022
         {
             switch (part)
             {
-                // case Core.Part.One:
-                //     return "v1";
-                // case Core.Part.Two:
-                //     return "v1";
+                case Core.Part.One:
+                    return "v1";
+                case Core.Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
         }
 
-        public override bool SkipTestData => false;
+        public override bool SkipTestData => true;
 
         protected override List<Core.TestDatum> GetTestData()
         {
             List<Core.TestDatum> testData = new List<Core.TestDatum>();
-            testData.Add(new Core.TestDatum
-            {
-                TestPart = Core.Part.One,
-                Output = "2=-1=0",
-                RawInput =
-@"1=-0-2
-12111
-2=0=
-21
-2=01
-111
-20012
-112
-1=-1=
-1-12
-12
-1=
-122"
-            });
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
@@ -97,13 +78,11 @@ namespace AoC._2022
 
             private long ProcessSnafu()
             {
-                // Core.Log.WriteLine(Core.Log.ELevel.Debug, $"Converting {Snafu}");
                 long sum = 0;
                 for (int i = Snafu.Length - 1; i >= 0; --i)
                 {
                     long digit = (long)Math.Pow(5, Snafu.Length - 1 - i);
                     sum += digit * Conversion[Snafu[i]];
-                    // Core.Log.WriteLine(Core.Log.ELevel.Debug, $"Sum={sum} ({Snafu[i]})| Digit ({digit}) [{Snafu.Length - 1 - i}]");
                 }
                 return sum;
             }
@@ -130,10 +109,9 @@ namespace AoC._2022
                 StringBuilder sb = new StringBuilder();
 
                 long working = Decimal;
-                while (index > 0)
+                while (index >= 0)
                 {
                     long maxIndexValue = GetMax(index - 1);
-                    //long curBase5 = (long)(Math.Pow(5, index));
                     long leftover = working % place;
                     if (leftover > maxIndexValue)
                     {
@@ -151,7 +129,7 @@ namespace AoC._2022
                     }
                     else if (leftover <  -1 * maxIndexValue)
                     {
-                        // negate
+                        // double negate
                         if (working / place < 0)
                         {
                             sb.Append('=');
@@ -185,15 +163,6 @@ namespace AoC._2022
                     --index;
                     place /= 5;
                 }
-
-                if (working < -2)
-                {
-                    sb.Append(BackwardsC[working + 5]);
-                }
-                else
-                {
-                    sb.Append(BackwardsC[working]);
-                }
                 return string.Join("", sb.ToString().TrimStart('0'));
             }
 
@@ -203,65 +172,8 @@ namespace AoC._2022
             }
         }
 
-        private void QuickPrintDecimal(Number n)
-        {
-            DebugWriteLine($"{n.Snafu} => {n.ToString()}");
-        }
-
-        private void QuickPrintSnafu(Number n)
-        {
-            DebugWriteLine($"{n.Decimal} => {n.ToString()}");
-        }
-
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            // DebugWriteLine($"0 @ {Number.GetMax(0)}");
-            // DebugWriteLine($"1 @ {Number.GetMax(1)}");
-            // DebugWriteLine($"2 @ {Number.GetMax(2)}");
-            // DebugWriteLine($"3 @ {Number.GetMax(3)}");
-            // DebugWriteLine($"4 @ {Number.GetMax(4)}");
-            // DebugWriteLine($"5 @ {Number.GetMax(5)}");
-
-            // // 1 - 5
-            // QuickPrintDecimal(new Number("1"));
-            // QuickPrintDecimal(new Number("2"));
-            // QuickPrintDecimal(new Number("1="));
-            // QuickPrintDecimal(new Number("1-"));
-            // QuickPrintDecimal(new Number("10"));
-
-            // // 11 - 15
-            // QuickPrintDecimal(new Number("21"));
-            // QuickPrintDecimal(new Number("22"));
-            // QuickPrintDecimal(new Number("1=="));
-            // QuickPrintDecimal(new Number("1=-"));
-            // QuickPrintDecimal(new Number("1=0"));
-
-            // // 21 - 25
-            // QuickPrintDecimal(new Number("1-1"));
-            // QuickPrintDecimal(new Number("1-2"));
-            // QuickPrintDecimal(new Number("10="));
-            // QuickPrintDecimal(new Number("10-"));
-            // QuickPrintDecimal(new Number("100"));
-
-            // // 31 - 35
-            // QuickPrintDecimal(new Number("111"));
-            // QuickPrintDecimal(new Number("112"));
-            // QuickPrintDecimal(new Number("12="));
-            // QuickPrintDecimal(new Number("12-"));
-            // QuickPrintDecimal(new Number("120"));
-
-
-            // QuickPrintSnafu(new Number(300)); // 2200
-            // QuickPrintSnafu(new Number(107)); // 1-12
-            // QuickPrintSnafu(new Number(1747)); // 1=-0-2
-            // QuickPrintSnafu(new Number(906)); // 12111
-            // QuickPrintSnafu(new Number(201)); // 2=01
-            // QuickPrintSnafu(new Number(198)); // 2=0=
-            // for (int i = 1; i <= 40; ++i)
-            // {
-            //     QuickPrintSnafu(new Number(i));
-            // }
-
             List<Number> numbers = inputs.Select(i => new Number(i)).ToList();
             Number fuel = new Number(numbers.Sum(n => n.Decimal));
             return fuel.Snafu;
