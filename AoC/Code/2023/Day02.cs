bea4b0a94b1d8d5010751677beceb91bb21dc568
mixco1a0 <mixco1a0@gmail.com>
2023-12-02 00:13:43 -0500

    d02 p1

--- AoC/Code/2023/Day02.cs
+++ AoC/Code/2023/Day02.cs
@@ -29,9 +29,13 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "8",
                 RawInput =
-@""
+@"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +47,58 @@ namespace AoC._2023
             return testData;
         }
 
+        public class BagOfCubes
+        {
+            public int Id { get; set; }
+            public int MaxBlue { get; set; }
+            public int MaxRed { get; set; }
+            public int MaxGreen { get; set; }
+
+            public bool IsValid(int b, int r, int g)
+            {
+                return MaxBlue <= b && MaxRed <= r && MaxGreen <= g;
+            }
+
+            public static BagOfCubes Parse(string input)
+            {
+                BagOfCubes boc = new BagOfCubes();
+                string[] inputs = input.Split(":;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+                boc.Id = int.Parse(string.Join("", inputs[0].Skip(5)));
+                foreach (string curI in inputs.Skip(1))
+                {
+                    string[] curISplits = curI.Split(" ,;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    int count = 0;
+                    foreach (string curISplit in curISplits)
+                    {
+                        if (char.IsAsciiDigit(curISplit[0]))
+                        {
+                            count = int.Parse(curISplit);
+                        }
+                        else
+                        {
+                            switch (curISplit[0])
+                            {
+                                case 'b':
+                                    boc.MaxBlue = Math.Max(boc.MaxBlue, count);
+                                    break;
+                                case 'r':
+                                    boc.MaxRed = Math.Max(boc.MaxRed, count);
+                                    break;
+                                case 'g':
+                                    boc.MaxGreen = Math.Max(boc.MaxGreen, count);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                return boc;
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<BagOfCubes> bocs = inputs.Select(BagOfCubes.Parse).Where(b => b.IsValid(14, 12, 13)).ToList();
+            return bocs.Select(b => b.Id).Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
