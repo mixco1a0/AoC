be09c46aa9030cb57478f4b60faab906878f19c5
mixco1a0 <mixco1a0@gmail.com>
2023-12-02 00:24:20 -0500

    d02 p2

--- AoC/Code/2023/Day02.cs
+++ AoC/Code/2023/Day02.cs
@@ -12,10 +12,10 @@ namespace AoC._2023
         {
             switch (part)
             {
-                // case Core.Part.One:
-                //     return "v1";
-                // case Core.Part.Two:
-                //     return "v1";
+                case Core.Part.One:
+                    return "v1";
+                case Core.Part.Two:
+                    return "v1";
                 default:
                     return base.GetSolutionVersion(part);
             }
@@ -40,9 +40,13 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.Two,
-                Output = "",
+                Output = "2286",
                 RawInput =
-@""
+@"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             });
             return testData;
         }
@@ -57,6 +61,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             public bool IsValid(int b, int r, int g)
             {
                 return MaxBlue <= b && MaxRed <= r && MaxGreen <= g;
+            }
+
+            public int GetPower()
+            {
+                return MaxBlue * MaxRed * MaxGreen;
             }
 
             public static BagOfCubes Parse(string input)
@@ -95,16 +104,23 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool findPossible)
         {
-            List<BagOfCubes> bocs = inputs.Select(BagOfCubes.Parse).Where(b => b.IsValid(14, 12, 13)).ToList();
-            return bocs.Select(b => b.Id).Sum().ToString();
+            IEnumerable<BagOfCubes> bocs = inputs.Select(BagOfCubes.Parse).ToList();
+            if (findPossible)
+            {
+                return bocs.Where(b => b.IsValid(14, 12, 13)).Select(b => b.Id).Sum().ToString();
+            }
+            else
+            {
+                return bocs.Select(b => b.GetPower()).Sum().ToString();
+            }
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
     }
 }
