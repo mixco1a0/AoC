fb8e3dd21e3e09f595d56bcfb33ef9d47a171cb9
mixco1a0 <mixco1a0@gmail.com>
2023-12-03 01:28:14 -0500

    d03 p2

--- AoC/Code/2023/Day03.cs
+++ AoC/Code/2023/Day03.cs
@@ -45,9 +45,18 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.Two,
-                Output = "",
+                Output = "467835",
                 RawInput =
-@""
+@"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."
             });
             return testData;
         }
@@ -123,33 +132,41 @@ namespace AoC._2023
             }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool findGears)
         {
             Schematic schematic = new Schematic(inputs);
             int runningTotal = 0;
-            foreach (Part part in schematic.Parts)
+            if (findGears)
             {
-                // IEnumerable<Base.Pos2> adjacent = schematic.Symbols.Where(s => s.Y == part.Col && (s.X == (part.Rows.Min - 1) || s.X == (part.Rows.Max + 1)));
-                Base.Range maxRange = new Base.Range(part.Rows.Min - 1, part.Rows.Max + 1);
-                IEnumerable<Symbol> aboveBelow = schematic.Symbols.Where(s => (Math.Abs(s.Pos.Y - part.Col) <= 1) && maxRange.HasInc(s.Pos.X));
-                if (aboveBelow.Any())
+                foreach (Symbol symbol in schematic.Symbols.Where(s => s.Character == '*'))
                 {
-                    runningTotal += part.Number;
-                    // string symbols = "";
-                    // foreach (Symbol s in aboveBelow)
-                    // {
-                    //     symbols += $"{s.Character}, ";
-                    // }
-                    // DebugWriteLine($"{part.Number} -> {symbols}");
+                    Base.Range maxRange = new Base.Range(symbol.Pos.X - 1, symbol.Pos.X + 1);
+                    IEnumerable<Part> touching = schematic.Parts.Where(p => Math.Abs(symbol.Pos.Y - p.Col) <= 1 && (maxRange.HasInc(p.Rows.Min) || maxRange.HasInc(p.Rows.Max)));
+                    if (touching.Count() == 2)
+                    {
+                        runningTotal += (touching.First().Number * touching.Last().Number);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Part part in schematic.Parts)
+                {
+                    Base.Range maxRange = new Base.Range(part.Rows.Min - 1, part.Rows.Max + 1);
+                    IEnumerable<Symbol> aboveBelow = schematic.Symbols.Where(s => (Math.Abs(s.Pos.Y - part.Col) <= 1) && maxRange.HasInc(s.Pos.X));
+                    if (aboveBelow.Any())
+                    {
+                        runningTotal += part.Number;
+                    }
                 }
             }
             return runningTotal.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
     }
 }
