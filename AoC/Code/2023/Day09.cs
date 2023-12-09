e53e3aec22526326d89212f318a1a828298bc163
mixco1a0 <mixco1a0@gmail.com>
2023-12-09 08:15:24 -0500

    d09 p1

--- AoC/Code/2023/Day09.cs
+++ AoC/Code/2023/Day09.cs
@@ -29,9 +29,11 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "114",
                 RawInput =
-@""
+@"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +45,66 @@ namespace AoC._2023
             return testData;
         }
 
+        public class Oasis
+        {
+            public List<long> Values { get; set; }
+            public long NextValue { get; set; }
+            public Action<string> PrintFunc { get; set; }
+
+            public Oasis()
+            {
+                PrintFunc = (_) => {};
+                Values = new List<long>();
+            }
+
+            public static Oasis Parse(string input)
+            {
+                return new Oasis() { Values = new List<long>(Util.Number.SplitL(input, ' ')) };
+            }
+
+            public void AddNextValue()
+            {
+                NextValue = WorkDown(Values);
+                PrintFunc($"{string.Join(',', Values)} -> {NextValue}");
+            }
+
+            public long WorkDown(List<long> prevHistory)
+            {
+                List<long> curHistory = new List<long>();
+                for (int i = 0; i < prevHistory.Count - 1; ++i)
+                {
+                    curHistory.Add(prevHistory[i + 1] - prevHistory[i]);
+                }
+
+                IEnumerable<long> distinctValues = curHistory.Distinct();
+                if (distinctValues.Count() == 1 && distinctValues.First() == 0)
+                {
+                    PrintFunc($"{string.Join(',', curHistory)} -> 0");
+                    return prevHistory.Last();
+                }
+                else
+                {
+                    long nextValue = WorkDown(curHistory);
+                    PrintFunc($"{string.Join(',', curHistory)} -> {nextValue}");
+                    return nextValue + prevHistory.Last();
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{string.Join(',', Values)} -> {NextValue}";
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<Oasis> allOasis = inputs.Select(Oasis.Parse).ToList();
+            foreach (Oasis oasis in allOasis)
+            {
+                //oasis.PrintFunc = DebugWriteLine;
+                oasis.AddNextValue();
+            }
+            return allOasis.Select(o => o.NextValue).Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
