1258a03abc281fe988414562203203ec15e6f5b7
mixco1a0 <mixco1a0@gmail.com>
2023-12-13 18:09:53 -0500

    d13 p1

--- AoC/Code/2023/Day13.cs
+++ AoC/Code/2023/Day13.cs
@@ -29,9 +29,23 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "405",
                 RawInput =
-@""
+@"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +57,66 @@ namespace AoC._2023
             return testData;
         }
 
+        private const char Ash = '.';
+        private const char Rock = '#';
+
+        private bool IsReflected(List<string> pattern, int index)
+        {
+            for (int i = index - 1, j = index + 2; i >= 0 && j < pattern.Count; --i, ++j)
+            {
+                if (pattern[i] != pattern[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int GetScore(List<string> pattern)
+        {
+            for (int i = 0; i < pattern.Count - 1; ++i)
+            {
+                if (pattern[i] == pattern[i + 1] && IsReflected(pattern, i))
+                {
+                    return 100 * (i + 1);
+                }
+            }
+
+            Util.Grid.RotateGrid(true, ref pattern);
+            for (int i = 0; i < pattern.Count - 1; ++i)
+            {
+                if (pattern[i] == pattern[i + 1] && IsReflected(pattern, i))
+                {
+                    return (i + 1);
+                }
+            }
+            return 0;
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<List<string>> patterns = new List<List<string>>();
+            List<string> cur = new List<string>();
+            foreach (string input in inputs)
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    patterns.Add(new List<string>(cur));
+                    cur.Clear();
+                }
+                else
+                {
+                    cur.Add(input);
+                }
+            }
+            patterns.Add(new List<string>(cur));
+
+            int score = 0;
+            foreach (List<string> pattern in patterns)
+            {
+                score += GetScore(pattern);
+            }
+            return score.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
