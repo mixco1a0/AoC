b1e8eebc191d4139c3c28118d8fe8c1e8c030442
mixco1a0 <mixco1a0@gmail.com>
2023-12-13 21:33:13 -0500

    d13 p2

--- AoC/Code/2023/Day13.cs
+++ AoC/Code/2023/Day13.cs
@@ -50,9 +50,23 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.Two,
-                Output = "",
+                Output = "400",
                 RawInput =
-@""
+@"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
             });
             return testData;
         }
@@ -60,23 +74,59 @@ namespace AoC._2023
         private const char Ash = '.';
         private const char Rock = '#';
 
-        private bool IsReflected(List<string> pattern, int index)
+        private bool IsReflected(List<string> pattern, int index, bool fixSmudge)
         {
-            for (int i = index - 1, j = index + 2; i >= 0 && j < pattern.Count; --i, ++j)
+            bool smudgeFixed = false;
+            for (int i = index, j = index + 1; i >= 0 && j < pattern.Count; --i, ++j)
             {
                 if (pattern[i] != pattern[j])
                 {
-                    return false;
+                    if (!fixSmudge)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (smudgeFixed)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            // attempt the smudge fix
+                            int diffCount = 0;
+                            for (int t = 0; t < pattern[i].Length && diffCount <= 1; ++t)
+                            {
+                                if (pattern[i][t] != pattern[j][t])
+                                {
+                                    ++diffCount;
+                                }
+                            }
+                            if (diffCount == 1)
+                            {
+                                smudgeFixed = true;
+                            }
+                            else if (diffCount > 1)
+                            {
+                                return false;
+                            }
+                        }
+                    }
                 }
+            }
+
+            if (fixSmudge && !smudgeFixed)
+            {
+                return false;
             }
             return true;
         }
 
-        private int GetScore(List<string> pattern)
+        private int GetScore(List<string> pattern, bool fixSmudge)
         {
             for (int i = 0; i < pattern.Count - 1; ++i)
             {
-                if (pattern[i] == pattern[i + 1] && IsReflected(pattern, i))
+                if (IsReflected(pattern, i, fixSmudge))
                 {
                     return 100 * (i + 1);
                 }
@@ -85,15 +135,16 @@ namespace AoC._2023
             Util.Grid.RotateGrid(true, ref pattern);
             for (int i = 0; i < pattern.Count - 1; ++i)
             {
-                if (pattern[i] == pattern[i + 1] && IsReflected(pattern, i))
+                if (IsReflected(pattern, i, fixSmudge))
                 {
                     return (i + 1);
                 }
             }
+            
             return 0;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool fixSmudge)
         {
             List<List<string>> patterns = new List<List<string>>();
             List<string> cur = new List<string>();
@@ -114,15 +165,15 @@ namespace AoC._2023
             int score = 0;
             foreach (List<string> pattern in patterns)
             {
-                score += GetScore(pattern);
+                score += GetScore(pattern, fixSmudge);
             }
             return score.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
     }
 }
