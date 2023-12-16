1bf02565310cd304db4b928170138d9c1aa3f7f3
mixco1a0 <mixco1a0@gmail.com>
2023-12-16 09:21:12 -0500

    d14 p1

--- AoC/Code/2023/Day14.cs
+++ AoC/Code/2023/Day14.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,9 +30,18 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "136",
                 RawInput =
-@""
+@"O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +53,59 @@ namespace AoC._2023
             return testData;
         }
 
+        private const char Round = 'O';
+        private const char Square = '#';
+        private const char Empty = '.';
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            // Util.Grid.PrintGrid(Core.Log.ELevel.Debug, inputs);
+            List<string> grid = new List<string>(inputs);
+            Util.Grid.RotateGrid(false, ref grid);
+            int load = grid.Count();
+            int totalLoad = 0;
+            List<string> shifted = new List<string>();
+            foreach (string g in grid)
+            {
+                // Core.TempLog.WriteLine($"before: {g}");
+                StringBuilder sb = new StringBuilder(g);
+                int emptyMove = -1;
+                // int curSquare = -1;
+                for (int i = 0; i < sb.Length; ++i)
+                {
+                    switch (sb[i])
+                    {
+                        case Round:
+                            if (emptyMove >= 0)
+                            {
+                                sb[emptyMove] = Round;
+                                sb[i] = Empty;
+                                totalLoad += (load - emptyMove);
+                                emptyMove++;
+                            }
+                            else
+                            {
+                                totalLoad += (load - i);
+                            }
+                            break;
+                        case Square:
+                            // curSquare = i;
+                            emptyMove = -1;
+                            break;
+                        case Empty:
+                            if (emptyMove == -1)
+                            {
+                                emptyMove = i;
+                            }
+                            break;
+                    }
+                }
+                shifted.Add(sb.ToString());
+                // Core.TempLog.WriteLine($"after:  {sb.ToString()}");
+            }
+            // Util.Grid.RotateGrid(true, ref shifted);
+            // Util.Grid.PrintGrid(Core.Log.ELevel.Debug, shifted);
+            return totalLoad.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
