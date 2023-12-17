c1ae8ca16dffcb7f92fb0b42f4e6610e5ec140af
mixco1a0 <mixco1a0@gmail.com>
2023-12-16 22:47:21 -0500

    d16 p1

--- AoC/Code/2023/Day16.cs
+++ AoC/Code/2023/Day16.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,9 +30,18 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Output = "46",
                 RawInput =
-@""
+@".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."
             });
             testData.Add(new Core.TestDatum
             {
@@ -43,9 +53,217 @@ namespace AoC._2023
             return testData;
         }
 
+        private enum Node { Empty = '.', Pos45 = '/', Neg45 = '\\', VSplit = '|', HSplit = '-' }
+
+        private enum Dir { North, South, East, West }
+        private record ReDirect(Dir Dir, Base.Pos2 Re);
+
+        private static Dictionary<Node, Dictionary<Dir, List<ReDirect>>> ReDirects = new Dictionary<Node, Dictionary<Dir, List<ReDirect>>>()
+        {
+            { Node.Empty, new Dictionary<Dir, List<ReDirect>>()
+            {
+                { Dir.North, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1))
+                    }
+                },
+                { Dir.South, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+                { Dir.East, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0))
+                    }
+                },
+                { Dir.West, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+            }},
+            { Node.Pos45, new Dictionary<Dir, List<ReDirect>>()
+            {
+                { Dir.North, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0))
+                    }
+                },
+                { Dir.South, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+                { Dir.East, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1))
+                    }
+                },
+                { Dir.West, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+            }},
+            { Node.Neg45, new Dictionary<Dir, List<ReDirect>>()
+            {
+                { Dir.North, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+                { Dir.South, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0))
+                    }
+                },
+                { Dir.East, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+                { Dir.West, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1))
+                    }
+                },
+            }},
+            { Node.VSplit, new Dictionary<Dir, List<ReDirect>>()
+            {
+                { Dir.North, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1))
+                    }
+                },
+                { Dir.South, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+                { Dir.East, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1)),
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+                { Dir.West, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.North, new Base.Pos2(0, -1)),
+                        new ReDirect(Dir.South, new Base.Pos2(0, 1))
+                    }
+                },
+            }},
+            { Node.HSplit, new Dictionary<Dir, List<ReDirect>>()
+            {
+                { Dir.North, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0)),
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+                { Dir.South, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0)),
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+                { Dir.East, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.East, new Base.Pos2(1, 0))
+                    }
+                },
+                { Dir.West, new List<ReDirect>()
+                    {
+                        new ReDirect(Dir.West, new Base.Pos2(-1, 0))
+                    }
+                },
+            }},
+        };
+
+        private record Target(Base.Pos2 Pos, Dir Direction);
+        // {
+        //     public override int GetHashCode()
+        //     {
+        //         return HashCode.Combine(Pos, Direction);
+        //     }
+        // }
+
+        private Node GetNode(char node)
+        {
+            switch (node)
+            {
+                case '/':
+                    return Node.Pos45;
+                case '\\':
+                    return Node.Neg45;
+                case '-':
+                    return Node.HSplit;
+                case '|':
+                    return Node.VSplit;
+                case '.':
+                default:
+                    return Node.Empty;
+            }
+        }
+
+        private void ParseInput(List<string> inputs, out Node[,] nodes)
+        {
+            nodes = new Node[inputs[0].Length, inputs.Count()];
+            for (int x = 0; x < inputs[0].Length; ++x)
+            {
+                for (int y = 0; y < inputs.Count; ++y)
+                {
+                    nodes[x, y] = GetNode(inputs[y][x]);
+                }
+            }
+        }
+
+        private void PrintGrid(Node[,] nodes)
+        {
+            StringBuilder sb = new StringBuilder();
+            Core.Log.WriteLine(Core.Log.ELevel.Debug, $"Printing grid {nodes.GetLength(0)}x{nodes.GetLength(1)}:");
+            for (int y = 0; y < nodes.GetLength(1); ++y)
+            {
+                sb.Clear();
+                sb.Append($"{y,4}| ");
+                sb.Append(string.Join(string.Empty, Enumerable.Range(0, nodes.GetLength(0)).Select(x => (char)nodes[x, y])));
+                Core.Log.WriteLine(Core.Log.ELevel.Debug, sb.ToString());
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            ParseInput(inputs, out Node[,] nodes);
+            PrintGrid(nodes);
+
+            int xMax = nodes.GetLength(0);
+            int yMax = nodes.GetLength(1);
+
+            HashSet<Target> visited = new HashSet<Target>();
+            Stack<Target> targets = new Stack<Target>();
+            targets.Push(new Target(new Base.Pos2(), Dir.East));
+            while (targets.Count > 0)
+            {
+                Target target = targets.Pop();
+                if (visited.Contains(target))
+                {
+                    continue;
+                }
+
+                visited.Add(target);
+
+                Node node = nodes[target.Pos.X, target.Pos.Y];
+                foreach (ReDirect reDirect in ReDirects[node][target.Direction])
+                {
+                    Base.Pos2 targetPos = target.Pos + reDirect.Re;
+                    if (targetPos.X >= 0 && targetPos.X < xMax && targetPos.Y >= 0 && targetPos.Y < yMax)
+                    {
+                        targets.Push(new Target(targetPos, reDirect.Dir));
+                    }
+                }
+            }
+            return visited.Select(t => t.Pos).Distinct().Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
