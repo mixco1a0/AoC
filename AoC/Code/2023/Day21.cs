6b79136d6de03d6df812a8e5a80b6fa86e0c457e
mixco1a0 <mixco1a0@gmail.com>
2023-12-21 21:59:22 -0500

    d21 p1

--- AoC/Code/2023/Day21.cs
+++ AoC/Code/2023/Day21.cs
@@ -29,9 +29,20 @@ namespace AoC._2023
             testData.Add(new Core.TestDatum
             {
                 TestPart = Core.Part.One,
-                Output = "",
+                Variables = new Dictionary<string, string> { { nameof(_Steps), "6" } },
+                Output = "16",
                 RawInput =
-@""
+@"...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+..........."
             });
             testData.Add(new Core.TestDatum
             {
@@ -42,10 +53,60 @@ namespace AoC._2023
             });
             return testData;
         }
+        private int _Steps { get; }
+
+        private static char Plot = '.';
+        // private static char Rock = '.';
+        private static char Start = 'S';
+        private static char Step = 'O';
+        static readonly Base.Pos2L[] GridMoves = new Base.Pos2L[] { new Base.Pos2L(0, 1), new Base.Pos2L(1, 0), new Base.Pos2L(-1, 0), new Base.Pos2L(0, -1) };
+
+        private void ParseInput(List<string> inputs, out char[,] grid, out Base.Pos2L start, out int xMax, out int yMax)
+        {
+            start = new Base.Pos2L();
+            grid = new char[inputs[0].Length, inputs.Count()];
+            for (int x = 0; x < inputs[0].Length; ++x)
+            {
+                for (int y = 0; y < inputs.Count; ++y)
+                {
+                    grid[x, y] = inputs[y][x];
+                    if (grid[x, y] == Start)
+                    {
+                        start = new Base.Pos2L(x, y);
+                        grid[x, y] = Plot;
+                    }
+                }
+            }
+            xMax = grid.GetLength(0);
+            yMax = grid.GetLength(1);
+        }
 
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            ParseInput(inputs, out char[,] grid, out Base.Pos2L start, out int xMax, out int yMax);
+            // Util.Grid.PrintGrid(grid);
+            GetVariable(nameof(_Steps), 64, variables, out int stepCount);
+            HashSet<Base.Pos2L> plots = new HashSet<Base.Pos2L>() { start };
+            for (int i = 0; i < stepCount; ++i)
+            {
+                HashSet<Base.Pos2L> newPlots = new HashSet<Base.Pos2L>();
+                char[,] copy = (char[,])grid.Clone();
+                foreach (Base.Pos2L plot in plots)
+                {
+                    foreach (Base.Pos2L gridMove in GridMoves)
+                    {
+                        Base.Pos2L newPlot = plot + gridMove;
+                        if (newPlot.X >= 0 && newPlot.X < xMax && newPlot.Y >= 0 && newPlot.Y < yMax && grid[newPlot.X, newPlot.Y] == Plot)
+                        {
+                            newPlots.Add(newPlot);
+                            copy[newPlot.X, newPlot.Y] = Step;
+                        }
+                    }
+                }
+                // Util.Grid.PrintGrid(copy);
+                plots = new HashSet<Base.Pos2L>(newPlots);
+            }
+            return plots.Count.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
