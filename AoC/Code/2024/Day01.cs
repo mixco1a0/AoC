1d4b4fabfcefc9ec6b04ba74b2ad9bdc290997a4
mixco1a0 <mixco1a0@gmail.com>
2024-12-01 00:19:10 -0500

    d01 p2

--- AoC/Code/2024/Day01.cs
+++ AoC/Code/2024/Day01.cs
@@ -12,8 +12,8 @@ namespace AoC._2024
         {
             return part switch
             {
-                // Core.Part.One => "v1",
-                // Core.Part.Two => "v1",
+                Core.Part.One => "v1",
+                Core.Part.Two => "v1",
                 _ => base.GetSolutionVersion(part),
             };
         }
@@ -37,15 +37,26 @@ namespace AoC._2024
                 new Core.TestDatum
                 {
                     TestPart = Core.Part.Two,
-                    Output = "",
+                    Output = "31",
                     RawInput =
-@""
+@"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
                 },
             ];
             return testData;
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private enum AnalyzeType
+        {
+            Distance,
+            Similarity
+        }
+
+        private static string SharedSolution(List<string> inputs, Dictionary<string, string> variables, AnalyzeType analyzeType)
         {
             List<int> left = [], right = [];
             foreach (string i in inputs)
@@ -54,20 +65,32 @@ namespace AoC._2024
                 left.Add(split[0]);
                 right.Add(split[1]);
             }
-            left.Sort();
-            right.Sort();
             int sum = 0;
-            for (int i = 0; i < left.Count; ++i)
+            if (analyzeType == AnalyzeType.Distance)
             {
-                sum += int.Abs(left[i] - right[i]);
+                left.Sort();
+                right.Sort();
+                for (int i = 0; i < left.Count; ++i)
+                {
+                    sum += int.Abs(left[i] - right[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < left.Count; ++i)
+                {
+                    int rCount = right.Where(r => r == left[i]).Count();
+                    int simScore = left[i] * rCount;
+                    sum += simScore;
+                }
             }
             return sum.ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, AnalyzeType.Distance);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, AnalyzeType.Similarity);
     }
 }
