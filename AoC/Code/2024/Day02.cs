3e6925833c606901b9a138d0933347c395403c62
mixco1a0 <mixco1a0@gmail.com>
2024-12-02 06:01:57 -0500

    d02 p1

--- AoC/Code/2024/Day02.cs
+++ AoC/Code/2024/Day02.cs
@@ -25,9 +25,14 @@ namespace AoC._2024
                 new Core.TestDatum
                 {
                     TestPart = Core.Part.One,
-                    Output = "",
+                    Output = "2",
                     RawInput =
-@""
+@"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
                 },
                 new Core.TestDatum
                 {
@@ -40,9 +45,79 @@ namespace AoC._2024
             return testData;
         }
 
+        private enum LevelType
+        {
+            Uninitialized,
+            None,
+            Increasing,
+            Decreasing
+        }
+
+        private class Report
+        {
+            public int[] Levels { get; set; }
+            public static Report Parse(string input)
+            {
+                return new Report { Levels = Util.String.Split(input, ' ').Select(int.Parse).ToArray() };
+            }
+
+            public bool IsSafe()
+            {
+                LevelType levelType = LevelType.Uninitialized;
+                int prev = 0;
+                bool isDiffSafe(int prev, int cur)
+                {
+                    int diff = int.Abs(prev - cur);
+                    return diff >= 1 && diff <= 3;
+                }
+                foreach (int level in Levels)
+                {
+                    switch (levelType)
+                    {
+                        case LevelType.Uninitialized:
+                            levelType = LevelType.None;
+                            break;
+                        case LevelType.None:
+                            if (isDiffSafe(prev, level))
+                            {
+                                if (level > prev)
+                                {
+                                    levelType = LevelType.Increasing;
+                                }
+                                else
+                                {
+                                    levelType = LevelType.Decreasing;
+                                }
+                            }
+                            else
+                            {
+                                // unsafe diff
+                                return false;
+                            }
+                            break;
+                        case LevelType.Increasing:
+                            if (level <= prev || !isDiffSafe(prev, level))
+                            {
+                                return false;
+                            }
+                            break;
+                        case LevelType.Decreasing:
+                            if (level >= prev || !isDiffSafe(prev, level))
+                            {
+                                return false;
+                            }
+                            break;
+                    }
+                    prev = level;
+                }
+                return true;
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            List<Report> reports = inputs.Select(Report.Parse).ToList();
+            return reports.Where(r => r.IsSafe()).Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
