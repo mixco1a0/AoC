c9b9b397a89d419ad9e62aec6e2a94ff5cb85ed0
mixco1a0 <mixco1a0@gmail.com>
2024-12-02 06:34:25 -0500

    d02 p2 partial

--- AoC/Code/2024/Day02.cs
+++ AoC/Code/2024/Day02.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using AoC.Core;
 
 namespace AoC._2024
 {
@@ -37,9 +38,14 @@ namespace AoC._2024
                 new Core.TestDatum
                 {
                     TestPart = Core.Part.Two,
-                    Output = "",
+                    Output = "4",
                     RawInput =
-@""
+@"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
                 },
             ];
             return testData;
@@ -49,6 +55,7 @@ namespace AoC._2024
         {
             Uninitialized,
             None,
+            NoneSkip,
             Increasing,
             Decreasing
         }
@@ -64,12 +71,14 @@ namespace AoC._2024
             public bool IsSafe()
             {
                 LevelType levelType = LevelType.Uninitialized;
-                int prev = 0;
-                bool isDiffSafe(int prev, int cur)
+
+                static bool isDiffSafe(int prev, int cur)
                 {
                     int diff = int.Abs(prev - cur);
                     return diff >= 1 && diff <= 3;
                 }
+
+                int prev = 0;
                 foreach (int level in Levels)
                 {
                     switch (levelType)
@@ -110,20 +119,139 @@ namespace AoC._2024
                     }
                     prev = level;
                 }
+
+                return true;
+            }
+
+            public bool IsDampenedSafe()
+            {
+                LevelType levelType = LevelType.Uninitialized;
+
+                static bool isDiffSafe(int prev, int cur)
+                {
+                    int diff = int.Abs(prev - cur);
+                    return diff >= 1 && diff <= 3;
+                }
+
+                int prev = 0;
+                int prevPrev = 0;
+                int unsafeCount = 0;
+                foreach (int level in Levels)
+                {
+                    switch (levelType)
+                    {
+                        case LevelType.Uninitialized:
+                            levelType = LevelType.None;
+                            break;
+                        case LevelType.None:
+                            if (isDiffSafe(prev, level))
+                            {
+                                if (level > prev)
+                                {
+                                    levelType = LevelType.Increasing;
+                                }
+                                else
+                                {
+                                    levelType = LevelType.Decreasing;
+                                }
+                            }
+                            else
+                            {
+                                ++unsafeCount;
+                                levelType = LevelType.NoneSkip;
+                            }
+                            break;
+                        case LevelType.NoneSkip:
+                            if (isDiffSafe(prev, level))
+                            {
+                                if (level > prev)
+                                {
+                                    levelType = LevelType.Increasing;
+                                }
+                                else
+                                {
+                                    levelType = LevelType.Decreasing;
+                                }
+                            }
+                            if (isDiffSafe(prevPrev, level))
+                            {
+                                if (level > prevPrev)
+                                {
+                                    levelType = LevelType.Increasing;
+                                }
+                                else
+                                {
+                                    levelType = LevelType.Decreasing;
+                                }
+                            }
+                            else
+                            {
+                                // two unsafe
+                                ++unsafeCount;
+                            }
+                            break;
+                        case LevelType.Increasing:
+                            if (level >= prev && isDiffSafe(prev, level))
+                            {
+                                // do nothing
+                            }
+                            else if (level >= prevPrev && isDiffSafe(prevPrev, level))
+                            {
+                                ++unsafeCount;
+                            }
+                            else
+                            {
+                                // more than one unsafe diff
+                                ++unsafeCount;
+                            }
+                            break;
+                        case LevelType.Decreasing:
+                            if (level <= prev && isDiffSafe(prev, level))
+                            {
+                                // do nothing
+                            }
+                            else if (level <= prevPrev && isDiffSafe(prevPrev, level))
+                            {
+                                ++unsafeCount;
+                            }
+                            else
+                            {
+                                // more than one unsafe diff
+                                ++unsafeCount;
+                            }
+                            break;
+                    }
+
+                    if (unsafeCount > 1)
+                    {
+                        Core.TempLog.WriteLine($"Unsafe => [{prevPrev}, {prev}, {level}] | {string.Join(' ', Levels)}");
+                        return false;
+                    }
+
+                    prevPrev = prev;
+                    prev = level;
+                }
+
                 return true;
             }
         }
 
-        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables, bool useProblemDampener)
         {
             List<Report> reports = inputs.Select(Report.Parse).ToList();
-            return reports.Where(r => r.IsSafe()).Count().ToString();
+            if (!useProblemDampener)
+            {
+                return reports.Where(r => r.IsSafe()).Count().ToString();
+            }
+            return reports.Where(r => r.IsSafe() || r.IsDampenedSafe()).Count().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, false);
 
         protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
-            => SharedSolution(inputs, variables);
+            => SharedSolution(inputs, variables, true);
+        // 340 => TOO LOW
+        // 372 => TOO LOW
     }
 }
