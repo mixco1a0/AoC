b45f823d25ee561e35fdea5945d9cebbcddfc046
mixco1a0 <mixco1a0@gmail.com>
2024-12-07 00:02:42 -0500

    d07 prep

---
+++ AoC/Code/2024/Day07.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC._2024
+{
+    class Day07 : Core.Day
+    {
+        public Day07() { }
+
+        public override string GetSolutionVersion(Core.Part part)
+        {
+            return part switch
+            {
+                // Core.Part.One => "v1",
+                // Core.Part.Two => "v1",
+                _ => base.GetSolutionVersion(part),
+            };
+        }
+
+        protected override List<Core.TestDatum> GetTestData()
+        {
+            List<Core.TestDatum> testData =
+            [
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.One,
+                    Output = "",
+                    RawInput =
+@""
+                },
+                new Core.TestDatum
+                {
+                    TestPart = Core.Part.Two,
+                    Output = "",
+                    RawInput =
+@""
+                },
+            ];
+            return testData;
+        }
+
+        private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
+        {
+            return string.Empty;
+        }
+
+        protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
+            => SharedSolution(inputs, variables);
+
+        protected override string RunPart2Solution(List<string> inputs, Dictionary<string, string> variables)
+            => SharedSolution(inputs, variables);
+    }
+}
