2a6ff7fc6929e663f3276822d2b48501d7b50e7d
mixco1a0 <mixco1a0@gmail.com>
2024-12-09 21:56:36 -0500

    d09 p1

--- AoC/Code/2024/Day09.cs
+++ AoC/Code/2024/Day09.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AoC._2024
 {
@@ -22,12 +23,19 @@ namespace AoC._2024
         {
             List<Core.TestDatum> testData =
             [
+//                 new Core.TestDatum
+//                 {
+//                     TestPart = Core.Part.One,
+//                     Output = "1",
+//                     RawInput =
+// @"12345"
+//                 },
                 new Core.TestDatum
                 {
                     TestPart = Core.Part.One,
-                    Output = "",
+                    Output = "1928",
                     RawInput =
-@""
+@"2333133121414131402"
                 },
                 new Core.TestDatum
                 {
@@ -40,9 +48,67 @@ namespace AoC._2024
             return testData;
         }
 
+        private readonly int FreeSpaceId = -1;
+
+        private void GetRawFileBlock(int[] ints, out List<int> rawFileBlock)
+        {
+            rawFileBlock = [];
+            bool isNumber = true;
+            int fileId = 0;
+            foreach (int i in ints)
+            {
+                if (isNumber)
+                {
+                    if (i == 0)
+                    {
+                        isNumber = false;
+                    }
+                    isNumber = false;
+                    rawFileBlock.AddRange(Enumerable.Range(0, i).Select(num => fileId));
+                    ++fileId;
+                }
+                else
+                {
+                    isNumber = true;
+                    if (i > 0)
+                    {
+                        rawFileBlock.AddRange(Enumerable.Range(0, i).Select(num => FreeSpaceId));
+                    }
+                }
+            }
+        }
+
+        private void GetCompressedFileBlock(List<int> rawFileBlock, out int[] compressedFileBlock)
+        {
+            int usedBlocks = rawFileBlock.Where(rfb => rfb != FreeSpaceId).Count();
+            compressedFileBlock = new int[usedBlocks];
+            for (int i = 0; i < compressedFileBlock.Length; ++i)
+            {
+                if (rawFileBlock[i] != FreeSpaceId)
+                {
+                    compressedFileBlock[i] = rawFileBlock[i];
+                }
+                else
+                {
+                    rawFileBlock.Reverse();
+                    while (rawFileBlock[0] == FreeSpaceId)
+                    {
+                        rawFileBlock.RemoveAt(0);
+                    }
+                    int last = rawFileBlock[0];
+                    rawFileBlock.RemoveAt(0);
+                    compressedFileBlock[i] = last;
+                    rawFileBlock.Reverse();
+                }
+            }
+        }
+
         private string SharedSolution(List<string> inputs, Dictionary<string, string> variables)
         {
-            return string.Empty;
+            int[] ints = inputs.First().ToCharArray().Select(a => a - '0').ToArray();
+            GetRawFileBlock(ints, out List<int> rawFileBlock);
+            GetCompressedFileBlock(rawFileBlock, out int[] compressedFileBlock);
+            return compressedFileBlock.Select((value, index) => (long)value * index).Sum().ToString();
         }
 
         protected override string RunPart1Solution(List<string> inputs, Dictionary<string, string> variables)
